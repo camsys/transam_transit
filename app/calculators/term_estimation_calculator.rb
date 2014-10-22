df30da0f82c91284197f97aae3f2006267d33fed
Lydia Chang <lchang@camsys.com>
2014-10-22 07:28:22 -0400

    refactored term estimation calculator

--- app/calculators/term_estimation_calculator.rb
+++ app/calculators/term_estimation_calculator.rb
@@ -15,32 +15,8 @@ class TermEstimationCalculator < ConditionEstimationCalculator
 
     years = asset.age
 
-    if asset.type_of? :vehicle
-      if years <= 3
-        return -1.75 / 3 * years + 5 + 1.75 / 3
-      else
-        return 3.29886 * Math.exp(-0.0178422 * years)
-      end
-    elsif asset.type_of? :rail_car
-      if years <= 2.5
-        return -3.75 / 5 * years + 5 + 3.75 / 5
-      else
-        return 4.54794 * Math.exp(-0.0204658 * years)
-      end
-    elsif asset.type_of? :support_facility
-      if years <= 18
-        return 5.08593 * Math.exp(-0.0196381 * years)
-      elsif years <= 19
-        return -2.08 / 5 * years + 11.236
-      else
-        return 3.48719 * Math.exp(-0.0042457 * years)
-      end
-    elsif asset.type_of? :transit_facility
-      return scaled_sigmoid(6.689 - 0.255 * years)
-
-    elsif asset.type_of? :fixed_guideway
-      return 4.94961 * Math.exp(-0.0162812 * years)
-
+    if eval_term_spline(asset,years)
+      return eval_term_spline(asset,years)
     else
       # If we don't have a term curve then default to a Straight Line Estimation
       slc = StraightLineEstimationCalculator.new
@@ -57,41 +33,18 @@ class TermEstimationCalculator < ConditionEstimationCalculator
     years = asset.age
     condition_threshold = asset.policy.condition_threshold
 
-    last_condition = calculate(asset)
+    last_condition = eval_term_spline(asset,years)
 
     while last_condition > condition_threshold
       years += 1
 
-      if asset.type_of? :vehicle
-        if years <= 3
-          last_condition = -1.75 / 3 * years + 5 + 1.75 / 3
-        else
-          last_condition = 3.29886 * Math.exp(-0.0178422 * years)
-        end
-      elsif asset.type_of? :rail_car
-        if years <= 2.5
-          last_condition = -3.75 / 5 * years + 5 + 3.75 / 5
-        else
-          last_condition = 4.54794 * Math.exp(-0.0204658 * years)
-        end
-      elsif asset.type_of? :support_facility
-        if years <= 18
-          last_condition = 5.08593 * Math.exp(-0.0196381 * years)
-        elsif years <= 19
-          last_condition = -2.08 / 5 * years + 11.236
-        else
-          last_condition = 3.48719 * Math.exp(-0.0042457 * years)
-        end
-      elsif asset.type_of? :transit_facility
-        last_condition = scaled_sigmoid(6.689 - 0.255 * years)
-
-      elsif asset.type_of? :fixed_guideway
-        last_condition = 4.94961 * Math.exp(-0.0162812 * years)
-
+      if eval_term_spline(asset,years)
+        last_condition = eval_term_spline(asset,years)
       else
         # If we don't have a term curve then default to a Straight Line Estimation
         slc = StraightLineEstimationCalculator.new
-        last_condition = slc.calculate(asset)
+        years = slc.last_servicable_year(asset)
+        break
       end
     end
 
@@ -100,13 +53,40 @@ class TermEstimationCalculator < ConditionEstimationCalculator
   end
 
   protected
+    def eval_term_spline(asset,years)
+      if asset.type_of? :vehicle
+        if years <= 3
+          return -1.75 / 3 * years + 5 + 1.75 / 3
+        else
+          return 3.29886 * Math.exp(-0.0178422 * years)
+        end
+      elsif asset.type_of? :rail_car
+        if years <= 2.5
+          return -3.75 / 5 * years + 5 + 3.75 / 5
+        else
+          return 4.54794 * Math.exp(-0.0204658 * years)
+        end
+      elsif asset.type_of? :support_facility
+        if years <= 18
+          return 5.08593 * Math.exp(-0.0196381 * years)
+        elsif years <= 19
+          return -2.08 / 5 * years + 11.236
+        else
+          return 3.48719 * Math.exp(-0.0042457 * years)
+        end
+      elsif asset.type_of? :transit_facility
+        return scaled_sigmoid(6.689 - 0.255 * years)
+
+      elsif asset.type_of? :fixed_guideway
+        return 4.94961 * Math.exp(-0.0162812 * years)
+      end
+
+    end
+
     def scaled_sigmoid(val)
       x = Math.exp(val)
       return x / (1.0 + x) * 4 + 1
     end
 
-    def scaled_sigmoid_inverse(val)
-      return Math.log((5 - val) / (val - 1))
-    end
 
 end
