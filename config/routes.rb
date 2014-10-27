cf156a6d5c98663ae42fc55248d6478e48f81384
Julian Ray <jray@camsys.com>
2014-10-27 12:31:09 -0400

    Move funding sources controller and views from Core

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :funding_sources do
+    collection do
+        get 'details'      
+    end
+  end
   
+  resources :grants do
+    resources :documents
+  end
+    
 end
