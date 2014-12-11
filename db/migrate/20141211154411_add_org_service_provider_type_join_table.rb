707f2a8555f6fb0f64db7e3c83dc03a239fa6752
Aaron Magil <amagil@camsys.com>
2014-12-11 10:58:37 -0500

    updating index names to be shorter and better

--- db/migrate/20141211154411_add_org_service_provider_type_join_table.rb
+++ db/migrate/20141211154411_add_org_service_provider_type_join_table.rb
@@ -1,8 +1,8 @@
 class AddOrgServiceProviderTypeJoinTable < ActiveRecord::Migration
   def change
     create_join_table :organizations, :service_provider_types do |t|
-      t.index :organization_id
-      t.index :service_provider_type_id
+      t.index :organization_id, :name => :organization_spt_idx1
+      t.index :service_provider_type_id, :name => :organization_spt_idx2
     end
   end
 end
