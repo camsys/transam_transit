e7aebefb553131f0d55aaebeda91c2cc15d7174f
Lydia Chang <mathmerized@gmail.com>
2015-01-12 15:38:34 -0500

    qualify strings in db migrations

--- db/migrate/20141209163809_create_fta_bus_mode_types.rb
+++ db/migrate/20141209163809_create_fta_bus_mode_types.rb
@@ -1,10 +1,10 @@
 class CreateFtaBusModeTypes < ActiveRecord::Migration
   def up
     create_table :fta_bus_mode_types do |t|
-    	t.boolean :active, :null => false
-    	t.string :code, :null => false
-    	t.string :name, :null => false
-    	t.text :description
+      t.string :name,      :limit => 64,   :null => false
+    	t.string :code,      :limit => 3,    :null => false
+    	t.text :description, :limit => 254,  :null => false
+      t.boolean :active,   :null => false
     end
   end
   def down
--- db/migrate/20150112125953_create_vehicle_rebuild_types.rb
+++ db/migrate/20150112125953_create_vehicle_rebuild_types.rb
@@ -1,9 +1,9 @@
 class CreateVehicleRebuildTypes < ActiveRecord::Migration
   def change
     create_table :vehicle_rebuild_types do |t|
-      t.boolean :active, :null => false
-      t.string :name, :null => false
-      t.text :description
+      t.string :name,      :limit => 64,   :null => false
+      t.text :description, :limit => 254,  :null => false
+      t.boolean :active,   :null => false
     end
 
     add_column :assets, :vehicle_rebuild_type_id, :integer,   :after => :vehicle_storage_method_type_id
