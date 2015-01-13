ad4fe575f0eadb18976e75f50703f0ef05469cdd
Lydia Chang <mathmerized@gmail.com>
2015-01-13 10:51:32 -0500

    [#85841296] rename team_number to ntd_id_number

--- app/models/fta_agency.rb
+++ app/models/fta_agency.rb
@@ -7,48 +7,48 @@
 #
 #------------------------------------------------------------------------------
 class FtaAgency < TransitAgency
-  
+
   #------------------------------------------------------------------------------
-  # Callbacks 
+  # Callbacks
   #------------------------------------------------------------------------------
   after_initialize :set_defaults
-  
+
   #------------------------------------------------------------------------------
-  # Associations 
+  # Associations
   #------------------------------------------------------------------------------
-  
+
   # must have a single FTA agency type
   belongs_to                :fta_agency_type
-  
+
   # must have a single service area type
   belongs_to                :fta_service_area_type
-  
+
   # Every transit agency has one or more fta mode types
   has_and_belongs_to_many   :fta_mode_types, :foreign_key => 'organization_id'
-      
+
   # every transit agency services a set of geographies
   has_and_belongs_to_many   :districts,      :foreign_key => 'organization_id', :join_table => 'organizations_districts'
-    
+
   #------------------------------------------------------------------------------
   # Validations
   #------------------------------------------------------------------------------
   validates               :fta_agency_type_id,        :presence => :true
   validates               :fta_service_area_type_id,  :presence => :true
-  #validates               :subrecipient_number,       :presence => :true
-  #validates               :team_number,               :presence => :true
-  
+  #validates              :subrecipient_number,       :presence => :true
+  validates_format_of     :ntd_id_number,             :allow_nil => true,   :with => /\A\d{4}\z/
+
   #------------------------------------------------------------------------------
   # Scopes
   #------------------------------------------------------------------------------
-  
-  # List of allowable form param hash keys  
+
+  # List of allowable form param hash keys
   FORM_PARAMS = [
     :fta_agency_type_id,
     :fta_service_area_type_id,
     :indian_tribe,
     :subrecipient_number,
-    :team_number,
-    :fta_mode_type_ids => [],    
+    :ntd_id_number,
+    :fta_mode_type_ids => [],
     :district_ids => []
   ]
 
@@ -57,18 +57,18 @@ class FtaAgency < TransitAgency
   # Class Methods
   #
   #------------------------------------------------------------------------------
-      
+
   def self.allowable_params
     FORM_PARAMS
   end
-            
+
   #------------------------------------------------------------------------------
   #
   # Instance Methods
   #
   #------------------------------------------------------------------------------
 
-    
+
   #------------------------------------------------------------------------------
   #
   # Protected Methods
@@ -80,7 +80,6 @@ class FtaAgency < TransitAgency
   def set_defaults
     super
     self.indian_tribe ||= false
-  end    
-  
+  end
+
 end
-      
