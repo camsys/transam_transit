d717748af356a879ea037be40a96777d8deae91c
Aaron Magil <amagil@camsys.com>
2015-01-13 16:52:45 -0500

    Putting Rail-related asset types and subtypes into rail.seed.rb file.

--- db/seeds/rail.seeds.rb
+++ db/seeds/rail.seeds.rb
@@ -4,38 +4,58 @@ load_rail_items ||= "true"
 
 if load_rail_items == "true"
 
-	is_mysql = (ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'mysql2')
+  asset_types = [
+    {:active => 1, :name => 'Rail Cars',                :description => 'Rail cars and LRVs',         :class_name => 'RailCar',           :map_icon_name => "orangeIcon",   :display_icon_name => "fa travelcon-subway"},
+    {:active => 1, :name => 'Locomotives',              :description => 'Locomotives',                :class_name => 'Locomotive',        :map_icon_name => "redIcon",      :display_icon_name => "fa travelcon-train"}
+  ]
 
-	asset_subtypes = [
-		{:active => 1, :belongs_to => 'asset_type',  :type => 'Stations/Stops/Terminals', :name => 'Commuter Rail Station', :image => 'commuter_rail_station.png', :description => 'Commuter Rail Station'},
-		{:active => 1, :belongs_to => 'asset_type',  :type => 'Maintenance Equipment',  :name => 'Rail Maintenance Equipment',      :image => 'pickup_truck.png',     :description => 'Rail Maintenance Equipment'},
-		{:active => 1, :belongs_to => 'asset_type',  :type => 'Signals/Signs',  :name => 'Train Control/Signal System',    :image => 'pickup_truck.png',     :description => 'Train Control/Signal Systems'}
-	]
+  puts "  Merging asset_types"
+  asset_types.each do |asset_type|
+    x = AssetType.new(row)
+    x.save!
+  end
 
-	asset_subtypes.each do |asset_subtype|
-		asset_type = AssetType.find_by(name: asset_subtype[:type])
-		AssetSubtype.find_or_create_by(:name => asset_subtype[:name]) do |new_asset_subtype|
-			new_asset_subtype.active = asset_subtype[:active]
-			new_asset_subtype.asset_type = asset_type
-			new_asset_subtype.image = asset_subtype[:image]
-			new_asset_subtype.description = asset_subtype[:description]
-			new_asset_subtype.asset_type = asset_type
-		end
-	end
+  asset_subtypes = [
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Stations/Stops/Terminals', :name => 'Commuter Rail Station', :image => 'commuter_rail_station.png', :description => 'Commuter Rail Station'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Maintenance Equipment',  :name => 'Rail Maintenance Equipment',      :image => 'pickup_truck.png',     :description => 'Rail Maintenance Equipment'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Signals/Signs',  :name => 'Train Control/Signal System',    :image => 'pickup_truck.png',     :description => 'Train Control/Signal Systems'},
 
-	policy_items = [
-		{:active => 1, :policy_id => 1, :asset_subtype => 'Commuter Rail Station', :max_service_life_months => 12*25, :replacement_cost => 1000000,  :pcnt_residual_value => 0, :replacement_ali_code => '11.32.02', :rehabilitation_ali_code => '11.34.02', :rehabilitation_cost => 100000, :extended_service_life_months => 12*4, :extended_service_life_miles => 0},
-		{:active => 1, :policy_id => 1, :asset_subtype => 'Rail Maintenance Equipment',     :max_service_life_months => 50,  :replacement_ali_code => '12.42.06',  :rehabilitation_ali_code => '12.44.06', :max_service_life_miles => 0,  :replacement_cost => 0, :pcnt_residual_value => 0, :rehabilitation_cost => 0, :extended_service_life_months => 12*0, :extended_service_life_miles => 0},
-		{:active => 1, :policy_id => 1, :asset_subtype => 'Train Control/Signal System',    :max_service_life_months => 100,  :replacement_ali_code => '11.62.01', :rehabilitation_ali_code => '11.64.01', :max_service_life_miles => 0,  :replacement_cost => 0, :pcnt_residual_value => 0, :rehabilitation_cost => 0, :extended_service_life_months => 12*0, :extended_service_life_miles => 0}
-	]
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Rail Cars', :name => 'Light Rail Car', :image => 'light_rail.png',  :description => 'Light rail car'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Rail Cars', :name => 'Heavy Rail Car', :image => 'heavy_rail.png',  :description => 'Heavy rail car'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Rail Cars', :name => 'Commuter Rail Self Propelled (Elec)', :image => 'light_rail.png',  :description => 'Light rail car'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Rail Cars', :name => 'Commuter Rail Self Propelled (Diesel)', :image => 'heavy_rail.png',  :description => 'Heavy rail car'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Rail Cars', :name => 'Commuter Rail Car Trailer', :image => 'heavy_rail.png',  :description => 'Heavy rail car'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Rail Cars', :name => 'Incline Railway Car', :image => 'heavy_rail.png',  :description => 'Heavy rail car'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Rail Cars', :name => 'Cable Car', :image => 'heavy_rail.png',  :description => 'Heavy rail car'},
 
-	policy_items.each do |policy_item|
-		asset_subtype = AssetSubtype.find_by(name: policy_item[:asset_subtype])
-		PolicyItem.find_or_create_by(:asset_subtype => asset_subtype) do |new_policy_item|
-			policy_item.except(:asset_subtype).keys.each do |key|
-				new_policy_item[key] = policy_item[key]
-			end
-		end
-	end
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Locomotives', :name => 'Commuter Locomotive Diesel',   :image => 'light_rail.png',  :description => 'Commuter Locomotive (Diesel)'},
+    {:active => 1, :belongs_to => 'asset_type',  :type => 'Locomotives', :name => 'Commuter Locomotive Electric', :image => 'heavy_rail.png',  :description => 'Commuter Locomotive (Electric)'}
+  ]
+
+  asset_subtypes.each do |asset_subtype|
+    asset_type = AssetType.find_by(name: asset_subtype[:type])
+    AssetSubtype.find_or_create_by(:name => asset_subtype[:name]) do |new_asset_subtype|
+      new_asset_subtype.active = asset_subtype[:active]
+      new_asset_subtype.asset_type = asset_type
+      new_asset_subtype.image = asset_subtype[:image]
+      new_asset_subtype.description = asset_subtype[:description]
+      new_asset_subtype.asset_type = asset_type
+    end
+  end
+
+  policy_items = [
+    {:active => 1, :policy_id => 1, :asset_subtype => 'Commuter Rail Station',            :max_service_life_months => 12*25, :replacement_cost => 1000000,  :pcnt_residual_value => 0, :replacement_ali_code => '11.32.02', :rehabilitation_ali_code => '11.34.02', :rehabilitation_cost => 100000, :extended_service_life_months => 12*4, :extended_service_life_miles => 0},
+    {:active => 1, :policy_id => 1, :asset_subtype => 'Rail Maintenance Equipment',     :max_service_life_months => 50,  :replacement_ali_code => '12.42.06',  :rehabilitation_ali_code => '12.44.06', :max_service_life_miles => 0,  :replacement_cost => 0, :pcnt_residual_value => 0, :rehabilitation_cost => 0, :extended_service_life_months => 12*0, :extended_service_life_miles => 0},
+    {:active => 1, :policy_id => 1, :asset_subtype => 'Train Control/Signal System',    :max_service_life_months => 100,  :replacement_ali_code => '11.62.01', :rehabilitation_ali_code => '11.64.01', :max_service_life_miles => 0,  :replacement_cost => 0, :pcnt_residual_value => 0, :rehabilitation_cost => 0, :extended_service_life_months => 12*0, :extended_service_life_miles => 0}
+  ]
+
+  policy_items.each do |policy_item|
+    asset_subtype = AssetSubtype.find_by(name: policy_item[:asset_subtype])
+    PolicyItem.find_or_create_by(:asset_subtype => asset_subtype) do |new_policy_item|
+      policy_item.except(:asset_subtype).keys.each do |key|
+        new_policy_item[key] = policy_item[key]
+      end
+    end
+  end
 
 end
