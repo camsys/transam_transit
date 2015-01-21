f2b1cb002ebd06b78d3b4f9fd712066c0464fb3e
Aaron Magil <amagil@camsys.com>
2015-01-20 21:00:33 -0500

    Fix buggy migration

--- db/migrate/20150121014546_extend_length_of_grant_number.rb
+++ db/migrate/20150121014546_extend_length_of_grant_number.rb
@@ -1,5 +1,5 @@
 class ExtendLengthOfGrantNumber < ActiveRecord::Migration
   def change
-    change_column :grants, :grant_number, :limit => 64, :null => false
+    change_column :grants, :grant_number, :string, :limit => 64, :null => false
   end
 end
