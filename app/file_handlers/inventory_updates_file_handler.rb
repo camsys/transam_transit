c330d0117d9d5489bcf728a53bd4e9ba38c181e1
Julian Ray <jray@camsys.com>
2015-04-16 17:48:13 -0400

    Revised and renamed inventory updates handlers and builders

--- app/file_handlers/inventory_updates_file_handler.rb
+++ app/file_handlers/inventory_updates_file_handler.rb
@@ -1,48 +1,48 @@
 #------------------------------------------------------------------------------
 #
-# StatusUpdatesFileHandler
+# InventoryUpdatesFileHandler
 #
-# Generic class for processing status updates for assets from a spreadsheet.
+# Generic class for processing inventory updates for assets from a spreadsheet.
 #
 # This processes a file that has been exported as a template from the TransAM
 # application
 #
 #------------------------------------------------------------------------------
-class StatusUpdatesFileHandler < AbstractFileHandler
-  
+class InventoryUpdatesFileHandler < AbstractFileHandler
+
   OBJECT_KEY_COL = 0
   ASSET_SUBTYPE_COL = 1
   ASSET_TAG_COL = 2
-  
+
   NUM_HEADER_ROWS = 2
   SHEET_NAME = "Updates"
-  
+
   # Perform the processing
   def process(upload)
-        
+
     @num_rows_processed = 0
     @num_rows_added = 0
     @num_rows_skipped = 0
     @num_rows_replaced = 0
     @num_rows_failed = 0
-    
+
     # Get the pertinent info from the upload record
     file_url = upload.file.url                # Usually stored on S3
     organization = upload.organization        # Organization who owns the assets
     system_user = User.find(1)                # System user is always the first user
-    
+
     add_processing_message(1, 'success', "Updating asset status from '#{upload.original_filename}'")
     add_processing_message(1, 'success', "Start time = '#{Time.now}'")
-    
+
     # Open the spreadsheet and start to process the asset events
     begin
-      
+
       reader = SpreadsheetReader.new(file_url)
       reader.open(SHEET_NAME)
-      
+
       Rails.logger.info "  File Opened."
       Rails.logger.info "  Num Rows = #{reader.num_rows}, Num Cols = #{reader.num_cols}, Num Header Rows = #{NUM_HEADER_ROWS}"
-      
+
       # Process each row
       count_blank_rows = 0
       first_row = NUM_HEADER_ROWS + 1
@@ -64,16 +64,16 @@ class StatusUpdatesFileHandler < AbstractFileHandler
           subtype_str = cells[ASSET_SUBTYPE_COL].to_s
           # asset tags are sometimes stored as numbers
           asset_tag   = cells[ASSET_TAG_COL].to_s
-          
+
           Rails.logger.info "  Processing row #{row}. Asset ID = '#{object_key}', Subtype = '#{subtype_str}', Asset Tag = '#{asset_tag}'"
-          asset = Asset.where('organization_id = ? AND object_key = ?', organization.id, object_key).first     
-          
+          asset = Asset.where('organization_id = ? AND object_key = ?', organization.id, object_key).first
+
           # Attempt to find the asset
           # complain if we cant find it
-          if asset.nil?     
+          if asset.nil?
             add_processing_message(2, 'warning', "Could not retrieve asset with ID '#{object_key}'.")
             @num_rows_failed += 1
-            next                                              
+            next
           else
             add_processing_message(1, 'success', "Processing row[#{row}]  Asset ID: '#{object_key}', Subtype: '#{subtype_str}', Asset Tag: '#{asset_tag}'")
           end
@@ -83,23 +83,23 @@ class StatusUpdatesFileHandler < AbstractFileHandler
           unless asset.asset_subtype.name == subtype_str
             add_processing_message(2, 'warning', "Mismatch on asset subtype. Found subtype '#{subtype_str}' expected '#{asset.asset_subtype.name}'. Skipping row.")
             @num_rows_failed += 1
-            next                         
+            next
           end
 
           # Check to see if this asset tag and subtype are the same
           unless asset.asset_tag == asset_tag
             add_processing_message(2, 'warning', "Mismatch on asset tag. Found tag '#{asset_tag}' expected '#{asset.asset_tag}'. Skipping row.")
             @num_rows_failed += 1
-            next                         
+            next
           end
-         
+
           # Make sure this row has data otherwise skip it
           if reader.empty?(4, 24)
             @num_rows_skipped += 1
             add_processing_message(2, 'info', "No data for row. Skipping.")
             next
           end
-          
+
           has_new_event = false
 
           # If all the validations have passed, type the asset
@@ -110,7 +110,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
           if asset.type_of? :vehicle or asset.type_of? :support_vehicle
             unless reader.empty?(4, 4) # Only Current Mileage field is required
               add_processing_message(2, 'success', 'Processing Mileage Report')
-              loader = MileageUpdateEventLoader.new            
+              loader = MileageUpdateEventLoader.new
               loader.process(asset, cells[4..6])
               if loader.errors?
                 row_errored = true
@@ -135,7 +135,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
 
           unless reader.empty?(5, 5)
             add_processing_message(2, 'success', 'Processing Condition Report')
-            loader = ConditionUpdateEventLoader.new            
+            loader = ConditionUpdateEventLoader.new
             loader.process(asset, cells[4..7])
             if loader.errors?
               row_errored = true
@@ -162,7 +162,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
           #### Replacement Report Block########################################
           unless reader.empty?(8, 8)
             add_processing_message(2, 'success', 'Processing Replacement/Rebuild Report')
-            loader = ReplacementUpdateEventLoader.new            
+            loader = ReplacementUpdateEventLoader.new
             loader.process(asset, cells[8..10])
             if loader.errors?
               row_errored = true
@@ -187,7 +187,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
 
           unless reader.empty?(9, 9)
             add_processing_message(2, 'success', 'Processing Rebuild Report')
-            loader = RehabilitationUpdateEventLoader.new            
+            loader = RehabilitationUpdateEventLoader.new
             loader.process(asset, cells[8..10])
             if loader.errors?
               row_errored = true
@@ -214,7 +214,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
           ##### Usage Metrics Report Block ####################################
           unless reader.empty?(11, 16)
             add_processing_message(2, 'success', 'Processing Usage Metrics Report')
-            loader = UsageUpdateEventLoader.new            
+            loader = UsageUpdateEventLoader.new
             loader.process(asset, cells[11..16])
             if loader.errors?
               row_errored = true
@@ -237,7 +237,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
             end
 
             unless reader.empty?(14, 14) # Only Usage Codes field is required
-              loader = UsageCodesUpdateEventLoader.new            
+              loader = UsageCodesUpdateEventLoader.new
               loader.process(asset, cells[11..16])
               if loader.errors?
                 row_errored = true
@@ -257,7 +257,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
               else
                 Rails.logger.info "Usage Code Update did not pass validation."
                 event.errors.full_messages.each { |e| add_processing_message(3, 'warning', e)}
-              end            
+              end
             end
           end
 
@@ -265,7 +265,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
           #### Operational Metrics Report Block ###############################
           unless reader.empty?(17, 17)
             add_processing_message(2, 'success', 'Processing Maintentance Provider Report')
-            loader = MaintenanceProviderUpdateEventLoader.new            
+            loader = MaintenanceProviderUpdateEventLoader.new
             loader.process(asset, cells[17..24])
             if loader.errors?
               row_errored = true
@@ -287,10 +287,10 @@ class StatusUpdatesFileHandler < AbstractFileHandler
               event.errors.full_messages.each { |e| add_processing_message(3, 'warning', e)}
             end
           end
-          
+
           unless reader.empty?(18, 18)
             add_processing_message(2, 'success', 'Processing Storage Method Report')
-            loader = StorageMethodUpdateEventLoader.new            
+            loader = StorageMethodUpdateEventLoader.new
             loader.process(asset, cells[17..24])
             if loader.errors?
               row_errored = true
@@ -315,7 +315,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
 
           unless reader.empty?(19, 22)
             add_processing_message(2, 'success', 'Processing Operational Metrics Report')
-            loader = OperationsUpdateEventLoader.new            
+            loader = OperationsUpdateEventLoader.new
             loader.process(asset, cells[17..24])
             if loader.errors?
               row_errored = true
@@ -345,7 +345,7 @@ class StatusUpdatesFileHandler < AbstractFileHandler
           end
         end
       end
-      
+
       @new_status = FileStatusType.find_by_name("Complete")
     rescue => e
       Rails.logger.warn "Exception caught: #{e.backtrace.join("\n")}"
@@ -353,16 +353,16 @@ class StatusUpdatesFileHandler < AbstractFileHandler
       raise e
     ensure
       reader.close unless reader.nil?
-    end  
+    end
 
     add_processing_message(1, 'success', "Processing Completed at  = '#{Time.now}'")
-        
+
   end
-    
+
   # Init
   def initialize(upload)
     super
     @upload = upload
   end
-  
+
 end
