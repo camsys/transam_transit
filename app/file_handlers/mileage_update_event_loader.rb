55f2eccb1f9201640ed4d246a4a825547506d6ea
Julian Ray <jray@camsys.com>
2015-06-14 11:36:02 -0400

    Move generic handlers to transit

--- app/file_handlers/mileage_update_event_loader.rb
+++ app/file_handlers/mileage_update_event_loader.rb
@@ -1,31 +1,31 @@
 #------------------------------------------------------------------------------
 #
-# MileageUpdateEventLoader
+# Mileage Update Event
 #
-# Generic class for processing mileage update events
+# Used to process both SOGR updates and new inventory updates
 #
 #------------------------------------------------------------------------------
 class MileageUpdateEventLoader < EventLoader
-  
-  MILEAGE_COL           = 0
-  EVENT_DATE_COL        = 2 
-    
+
+  CURRENT_MILEAGE_COL   = 0
+  EVENT_DATE_COL        = 1
+
   def process(asset, cells)
 
     # Create a new MileageUpdateEvent
     @event = asset.build_typed_event(MileageUpdateEvent)
 
+    # Current Mileage
+    @event.current_mileage = as_integer(cells[CURRENT_MILEAGE_COL])
+
     # Event Date
     @event.event_date = as_date(cells[EVENT_DATE_COL])
-    
-    # Current Mileage
-    @event.current_mileage = as_integer(cells[MILEAGE_COL])
-    
+
   end
-  
+
   private
   def initialize
     super
   end
-  
+
 end
