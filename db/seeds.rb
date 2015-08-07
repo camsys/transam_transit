4250167de1247ae69ff9746d8c50f5eec6514a75
Sathyan1983 <Sathyan.Mathai@gmail.com>
2015-08-07 14:30:22 -0400

    Changed the role for the 'Asset Type by Organization' to admin. [#100836888]

--- db/seeds.rb
+++ db/seeds.rb
@@ -522,7 +522,7 @@ reports = [
     :view_name => "generic_report_table",
     :show_in_nav => 0,
     :show_in_dashboard => 0,
-    :roles => 'user',
+    :roles => 'admin',
     :description => 'Displays a summary of asset types by agency.',
     :custom_sql => "SELECT c.short_name AS 'Org', b.name AS 'Type', COUNT(*) AS 'Count' FROM assets a LEFT JOIN asset_subtypes b ON a.asset_subtype_id = b.id LEFT JOIN organizations c ON a.organization_id = c.id GROUP BY a.organization_id, a.asset_subtype_id ORDER BY c.short_name, b.name"},
   {:active => 1, :belongs_to => 'report_type', :type => "Planning Report",
