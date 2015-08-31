c2f6eb207b21dec09d64823e021d133938cdeb07
Lydia Chang <mathmerized@gmail.com>
2015-08-31 14:41:58 -0400

    update calculator for new policy analyzer

--- app/calculators/service_life_transit_calculator.rb
+++ app/calculators/service_life_transit_calculator.rb
@@ -11,19 +11,20 @@ class ServiceLifeTransitCalculator < ServiceLifeCalculator
   # Calculate the service life based on the minimum miles if the
   # asset has a maximum number of miles set
   def by_mileage(asset)
-    
+
     if asset.type_of? :vehicle or asset.type_of? :support_vehicle
+
       # Iterate over all the mileage update events from earliest to latest
       # and find the first year (if any) that the  policy replacement became
       # effective
-      policy_item = asset.policy_rule
-      if policy_item.max_service_life_miles
+      max_service_life_miles = asset.policy_analyzer.get_max_service_life_miles
+      if max_service_life_miles
         events = asset.mileage_updates(true)
         Rails.logger.debug "Found #{events.count} events."
-        Rails.logger.debug "max_service_life_miles = #{policy_item.max_service_life_miles}."
+        Rails.logger.debug "max_service_life_miles = #{max_service_life_miles}."
         events.each do |event|
           Rails.logger.debug "Event date = #{event.event_date}, Mileage = #{event.current_mileage}"
-          if event.current_mileage >= policy_item.max_service_life_miles
+          if event.current_mileage >= max_service_life_miles
             Rails.logger.debug "returning #{fiscal_year_year_on_date(event.event_date)}"
             return fiscal_year_year_on_date(event.event_date)
           end
