3b613c1d7c9ad7f91601bc5e08e86b65675f23cc
Lydia Chang <mathmerized@gmail.com>
2015-09-03 10:09:10 -0400

    update calculator for new db column name

--- app/calculators/service_life_transit_calculator.rb
+++ app/calculators/service_life_transit_calculator.rb
@@ -17,14 +17,14 @@ class ServiceLifeTransitCalculator < ServiceLifeCalculator
       # Iterate over all the mileage update events from earliest to latest
       # and find the first year (if any) that the  policy replacement became
       # effective
-      max_service_life_miles = asset.policy_analyzer.get_max_service_life_miles
-      if max_service_life_miles
+      min_service_life_miles = asset.policy_analyzer.get_min_service_life_miles
+      if min_service_life_miles
         events = asset.mileage_updates(true)
         Rails.logger.debug "Found #{events.count} events."
-        Rails.logger.debug "max_service_life_miles = #{max_service_life_miles}."
+        Rails.logger.debug "min_service_life_miles = #{min_service_life_miles}."
         events.each do |event|
           Rails.logger.debug "Event date = #{event.event_date}, Mileage = #{event.current_mileage}"
-          if event.current_mileage >= max_service_life_miles
+          if event.current_mileage >= min_service_life_miles
             Rails.logger.debug "returning #{fiscal_year_year_on_date(event.event_date)}"
             return fiscal_year_year_on_date(event.event_date)
           end
