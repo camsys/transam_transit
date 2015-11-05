514e0e1c2d493370c37ed9a76678f92a2ce57bbc
Lydia Chang <mathmerized@gmail.com>
2015-11-05 14:19:16 -0500

    rspec for transit controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require 'rspec/rails'
 require 'transam_transit'
 require 'factory_girl_rails'
 require 'database_cleaner'
+require 'devise'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -22,6 +23,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, :type => :controller
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
 end
