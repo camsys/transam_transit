5c8ff6803b9a163980e136d0060e97bc2b0823be
Lydia Chang <mathmerized@gmail.com>
2015-12-01 09:02:44 -0500

    remove accidental unnecessary code

--- spec/models/maintenance_provider_update_event_spec.rb
+++ spec/models/maintenance_provider_update_event_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe MaintenanceProviderUpdateEvent, :type => :model do
 
   let(:test_event) { create(:buslike_asset, :maintenance_provider_type => create(:maintenance_provider_type)).maintenance_provider_updates.create!(attributes_for(:maintenance_provider_update_event)) }
 
-  let(:test_asset) { create(:buslike_asset, ) }
-
   describe 'associations' do
     it 'has a type' do
       expect(MaintenanceProviderUpdateEvent.column_names).to include('maintenance_provider_type_id')
