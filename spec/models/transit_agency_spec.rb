5bb09df3501b754912fa355b9c08a440b4f6e93d
Lydia Chang <mathmerized@gmail.com>
2016-01-05 11:00:39 -0500

    move transit agency rspec from bpt to engine

---
+++ spec/models/transit_agency_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe TransitAgency, :type => :model do
+
+  let(:test_agency) { create(:transit_agency) }
+
+  it '.has_assets? works as expected' do
+    bus = build(:bus, organization_id: test_agency.id)
+    test_agency.assets << bus
+    expect(test_agency.has_assets?).to eql(true)
+  end
+
+  it '.asset_count works as expected' do
+    bus1 = create(:bus, organization_id: test_agency.id)
+    test_agency.assets << bus1
+    expect(test_agency.asset_count).to eql(1)
+    bus2 = create(:bus, organization_id: test_agency.id, serial_number: 'abcdefgh982347')
+    test_agency.assets << bus2
+
+    expect(test_agency.asset_count).to eql(2)
+  end
+
+end
