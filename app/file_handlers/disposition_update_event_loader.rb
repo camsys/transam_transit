bacaed534dd1680ba5362394491af3378f1f5bba
Lydia Chang <mathmerized@gmail.com>
2016-03-14 09:00:26 -0400

    [##115447497] transit dispositien updates templates

---
+++ app/file_handlers/disposition_update_event_loader.rb
@@ -0,0 +1,43 @@
+#------------------------------------------------------------------------------
+#
+# DispositionUpdateEventLoader
+#
+# Generic class for processing usage update events
+#
+#------------------------------------------------------------------------------
+class DispositionUpdateEventLoader < EventLoader
+
+  EVENT_DATE_COL        = 0
+  DISPOSITION_TYPE_COL  = 1
+  SALES_PROCEEDS_COL    = 2
+  MILEAGE_COL           = 3
+
+  def process(asset, cells)
+
+    # Create a new DispositionUpdateEvent
+    @event = asset.build_typed_event(DispositionUpdateEvent)
+
+    # Event Date
+    @event.event_date = as_date(cells[EVENT_DATE_COL])
+
+    # Disposition Type
+    val = as_string(cells[DISPOSITION_TYPE_COL])
+    @event.disposition_type = DispositionType.search(val)
+    if @event.disposition_type.nil?
+      @errors << "Dispositon Type not found or missing. Type = #{val}."
+    end
+
+    # Sales Proceeds
+    @event.sales_proceeds = as_integer(cells[SALES_PROCEEDS_COL])
+
+    # Mileage at disposition
+    @event.mileage_at_disposition = as_integer(cells[MILEAGE_COL]) if cells[MILEAGE_COL]
+
+  end
+
+  private
+  def initialize
+    super
+  end
+
+end
