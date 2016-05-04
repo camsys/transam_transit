8dd2a7703cf921c17c2e926dfcf3c1ec578db4ec
mmaranda-cs <mmaranda@camsys.com>
2016-05-04 17:00:19 -0400

    [#116538279] Removing the bulk disposition update option Transferred

--- app/file_handlers/transit_disposition_updates_template_builder.rb
+++ app/file_handlers/transit_disposition_updates_template_builder.rb
@@ -51,7 +51,7 @@ class TransitDispositionUpdatesTemplateBuilder < TemplateBuilder
     row = []
     @disposition_types = DispositionType.active.pluck(:name)
     @disposition_types.each do |x|
-      row << x unless x.eql? "Unknown"
+      row << x unless x.eql? "Unknown" or x.eql? "Transferred"
     end
     sheet.add_row row
 
