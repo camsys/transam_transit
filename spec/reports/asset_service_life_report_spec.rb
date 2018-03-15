04904f84725db4231fee89188c97c8523091055b
Taylor Liss <tliss@camsys.com>
2018-03-15 15:26:17 -0400

    Setting up assets correctly

--- spec/reports/asset_service_life_report_spec.rb
+++ spec/reports/asset_service_life_report_spec.rb
@@ -1,30 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AssetServiceLifeReport, :type => :report do
+  let(:test_agency) { create(:transit_operator) }
 
-  describe 'summary' do
-
-    it "can check equality" do
-      two = 2
-      expect(two).to eq(2)
-    end
-
-    it 'calculates correct number of assets past condition' do
-      organization = build(:organization)
-      asset_a = build(:buslike_asset)
-      report = AssetServiceLifeReport.new
-      # asset_a = Asset.new(organization: organization, reported_condition_rating: 1)
-      # asset_b = Asset.new(organization: 1, reported_condition_rating: 2)
-      # asset_c = Asset.new(organization: 1, reported_condition_rating: 3)
-      # assets = [asset_a, asset_b, asset_c]
-      assets = [asset_a]
-      organization_id_list = assets.map{|a| a.organization_id}
-      expect(report.get_data(organization_id_list, 0).past_esl_date).to eq(2)
-    end
-
-
-
+  it "can check equality" do
+    two = 2
+    expect(two).to eq(2)
   end
+
+  it 'calculates correct number of assets past condition' do
+
+    bus = build(:bus, organization_id: test_agency.id)
+
+    test_agency.assets << bus
+    expect(test_agency.has_assets?).to eql(true)
+
+    # organization = build(:organization)
+    # asset_a = build(:buslike_asset)
+    # report = AssetServiceLifeReport.new
+    # asset_a = Asset.new(organization: organization, reported_condition_rating: 1)
+    # asset_b = Asset.new(organization: 1, reported_condition_rating: 2)
+    # asset_c = Asset.new(organization: 1, reported_condition_rating: 3)
+    # assets = [asset_a, asset_b, asset_c]
+    assets = [bus]
+    organization_id_list = assets.map{|a| a.organization_id}
+    p organization_id_list
+    expect(report.get_data(organization_id_list, 0).past_esl_date).to eq(2)
+  end
+
 end
 
 # car1.reported_condition_rating = 1
