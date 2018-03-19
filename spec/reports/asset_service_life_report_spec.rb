0e24680c2834e2e4ef9734e86840ede7d3f97812
Taylor Liss <tliss@camsys.com>
2018-03-19 16:04:47 -0400

    add test to calculate total number of assets

--- spec/reports/asset_service_life_report_spec.rb
+++ spec/reports/asset_service_life_report_spec.rb
@@ -3,14 +3,33 @@ require 'rails_helper'
 RSpec.describe AssetServiceLifeReport, :type => :report do
   let(:test_agency) { create(:transit_operator) }
 
-  it 'calculates correct number of assets past ESL condition threshold' do
-    bus = build(:bus, organization_id: test_agency.id, reported_condition_rating: 5.0)
-    test_asset_subtype = Asset.new_asset(bus.asset_subtype)
+  it 'calculates the total number of assets' do
+    @organization = create(:organization)
+    parent_policy = create(:policy, :organization => create(:organization))
+    create(:policy_asset_type_rule, :policy => parent_policy, :asset_type => AssetType.first)
+    create(:policy_asset_subtype_rule, :policy => parent_policy, :asset_subtype => AssetSubtype.first)
+    policy = create(:policy, :organization => @organization, :parent => parent_policy)
+    create(:policy_asset_type_rule, :policy => policy, :asset_type => AssetType.first)
+    create(:policy_asset_subtype_rule, :policy => policy, :asset_subtype => AssetSubtype.first)
 
+    bus_a = create(:bus,
+                   {:organization => @organization,
+                    :asset_type => AssetType.first,
+                    :asset_subtype => AssetSubtype.first,
+                    :serial_number => "bus_a",
+                    :reported_condition_rating => 5.0})
+
+    bus_b = create(:bus,
+                  {:organization => @organization,
+                   :asset_type => AssetType.first,
+                   :asset_subtype => AssetSubtype.first,
+                   :reported_condition_rating => 5.0})
+
+    test_asset_subtype = Asset.new_asset(bus_a.asset_subtype)
     test_asset_type_id = test_asset_subtype.asset_type_id
     test_asset_subtype_id = test_asset_subtype.asset_subtype_id
 
-    assets = [bus]
+    assets = [bus_a, bus_b]
     organization_id_list = assets.map{|asset| asset.organization_id}
 
     test_months_past_esl_min = 0
@@ -22,8 +41,12 @@ RSpec.describe AssetServiceLifeReport, :type => :report do
                                                   :months_past_esl_min => test_months_past_esl_min,
                                                   :months_past_esl_max => test_months_past_esl_max})
 
-    total_assets_past_condition = report[:data][0][3]
+    total_assets = report[:data].last[2]
 
-    expect(total_assets_past_condition).to eq(0)
+    expect(total_assets).to eq(assets.size)
   end
+
+    # it 'calculates the number of assets past policy's ESL condition threshold' do
+    #
+    # end
 end
