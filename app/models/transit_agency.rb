b64a72fca23d72e1898650b5f7e776a16d7a7939
mathmerized <mathmerized@gmail.com>
2018-03-23 11:55:25 -0400

    use class name to pull organization type instead of name

--- app/models/transit_agency.rb
+++ app/models/transit_agency.rb
@@ -85,7 +85,7 @@ class TransitAgency < Organization
   end
   
   def group_lead_candidates
-    planning_partner_type_id = OrganizationType.where(name: "Planning Partner").pluck(:id)
+    planning_partner_type_id = OrganizationType.where(class_name: "PlanningPartner").pluck(:id)
 
     users.includes(:organization).with_any_role(:transit_manager, :manager, :guest)
       .select{|u| (!u.has_role?(:guest) ||
