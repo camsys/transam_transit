c55d4d40eb5a130081e0302a4b91fe796a410abc
mathmerized <mathmerized@gmail.com>
2018-04-12 14:11:41 -0400

    TTPLAT-149 TTPLAT-171 cleanup tam service life report when no ulbs activated for detail data

--- app/reports/asset_tam_policy_service_life_report.rb
+++ app/reports/asset_tam_policy_service_life_report.rb
@@ -98,25 +98,25 @@ class AssetTamPolicyServiceLifeReport < AbstractReport
         unless params[:years_past_esl_min].to_i > 0
           params[:years_past_esl_min] = 0
         end
-        @clauses[:years_past_esl_min] = params[:years_past_esl_min]
         past_ulb_counts = query.distinct.where('ulbs.useful_life_benchmark + IFNULL(sum_extended_eul, 0)/12 - (YEAR(CURDATE()) - assets.manufacture_year) >= ?', params[:years_past_esl_min].to_i)
+
         if params[:years_past_esl_max].to_i > 0
           past_ulb_counts = past_ulb_counts.distinct.where('12*(ulbs.useful_life_benchmark + IFNULL(sum_extended_eul, 0)/12 - (YEAR(CURDATE()) - assets.manufacture_year)) <= ?', params[:years_past_esl_max].to_i)
-          @clauses[:years_past_esl_max] = params[:years_past_esl_max]
         end
-        past_ulb_counts = past_ulb_counts.count('assets.id')
       else
-        past_ulb_counts = Hash.new
+        past_ulb_counts = query.none
       end
       if fta_asset_category.name == 'Revenue Vehicles'
+        past_ulb_counts = past_ulb_counts.group('CONCAT(fta_vehicle_types.code," - " ,fta_vehicle_types.name)')
         query = query.group('CONCAT(fta_vehicle_types.code," - " ,fta_vehicle_types.name)')
       else
+        past_ulb_counts = past_ulb_counts.group("#{asset_level_class}.name")
         query = query.group("#{asset_level_class}.name")
       end
 
       # Generate queries for each column
       asset_counts = query.distinct.count('assets.id')
-
+      past_ulb_counts = past_ulb_counts.count('assets.id')
       total_age = query.sum('YEAR(CURDATE()) - assets.manufacture_year')
       total_mileage = query.sum(:reported_mileage)
       total_condition = query.sum(:reported_condition_rating)
