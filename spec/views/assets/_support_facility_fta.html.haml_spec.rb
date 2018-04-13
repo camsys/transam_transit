2ddd58e5a63fac926b802beb63b2c5c3f70b8bf7
Taylor Liss <tliss@camsys.com>
2018-04-13 10:45:41 -0400

    Add extra tests to support_facility_fta

--- spec/views/assets/_support_facility_fta.html.haml_spec.rb
+++ spec/views/assets/_support_facility_fta.html.haml_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "assets/_support_facility_fta.html.haml", :type => :view do
 
-  it 'fta info' do
-    test_asset = create(:administration_building, :fta_funding_type_id => 1, :pcnt_capital_responsibility =>22, :fta_facility_type_id => 1, :facility_capacity_type_id => 1)
-    test_asset.fta_mode_types << FtaModeType.first
+  it 'fta info', do
+    test_asset = create(:administration_building, :fta_funding_type_id => 1, :pcnt_capital_responsibility =>22, :fta_facility_type_id => 1, :facility_capacity_type_id => 1, :primary_fta_mode_type => FtaModeType.first, :fta_private_mode_type => FtaPrivateModeType.first)
+    test_asset.secondary_fta_mode_types << FtaModeType.second
     test_asset.save!
     assign(:asset, test_asset)
     render
@@ -12,6 +12,8 @@ describe "assets/_support_facility_fta.html.haml", :type => :view do
     expect(rendered).to have_content(FtaFundingType.first.to_s)
     expect(rendered).to have_content('22%')
     expect(rendered).to have_content(FtaModeType.first.to_s)
+    expect(rendered).to have_content(FtaModeType.second.to_s)
+    expect(rendered).to have_content(FtaPrivateModeType.first.to_s)
     expect(rendered).to have_content(FtaFacilityType.first.to_s)
     expect(rendered).to have_content(FacilityCapacityType.first.to_s)
   end
