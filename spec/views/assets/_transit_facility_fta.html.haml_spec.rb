a992ab09148f4ba58b3054d9cbdeb26d4bae639d
Taylor Liss <tliss@camsys.com>
2018-04-12 20:25:10 -0400

    Update transit_facility_fta tests based on f2460cc, e4fadaf, and 6729a66

--- spec/views/assets/_transit_facility_fta.html.haml_spec.rb
+++ spec/views/assets/_transit_facility_fta.html.haml_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe "assets/_transit_facility_fta.html.haml", :type => :view do
 
   it 'fta info' do
-    test_asset = create(:bus_shelter, :fta_funding_type_id => 1, :pcnt_capital_responsibility =>22, :fta_facility_type_id => 1)
-    test_asset.fta_mode_types << FtaModeType.first
+    test_asset = create(:bus_shelter, :fta_funding_type_id => 1, :pcnt_capital_responsibility =>22, :fta_facility_type_id => 1, :primary_fta_mode_type => FtaModeType.first, :fta_private_mode_type => FtaPrivateModeType.first)
+    test_asset.secondary_fta_mode_types << FtaModeType.second
     test_asset.save!
     assign(:asset, test_asset)
     render
@@ -12,6 +12,8 @@ describe "assets/_transit_facility_fta.html.haml", :type => :view do
     expect(rendered).to have_content(FtaFundingType.first.to_s)
     expect(rendered).to have_content('22%')
     expect(rendered).to have_content(FtaModeType.first.to_s)
+    expect(rendered).to have_content(FtaModeType.second.to_s)
+    expect(rendered).to have_content(FtaPrivateModeType.first.to_s)
     expect(rendered).to have_content(FtaFacilityType.first.to_s)
   end
 end
