124902b0c395f03e8882d689ae4eb86a984b1aed
Taylor Liss <tliss@camsys.com>
2018-04-16 10:09:44 -0400

    Update tests for transit_facility_fta to test for primary and secondary mode types

--- spec/views/assets/_transit_facility_fta.html.haml_spec.rb
+++ spec/views/assets/_transit_facility_fta.html.haml_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe "assets/_transit_facility_fta.html.haml", :type => :view do
 
   it 'fta info' do
-    test_asset = create(:bus_shelter, :fta_funding_type_id => 1, :pcnt_capital_responsibility =>22, :fta_facility_type_id => 1, :primary_fta_mode_type => FtaModeType.first, :fta_private_mode_type => FtaPrivateModeType.first)
+    test_asset = create(:bus_shelter, :fta_funding_type_id => 1, :pcnt_capital_responsibility =>22, :fta_facility_type_id => FtaFacilityType.first.id, :primary_fta_mode_type => FtaModeType.first, :fta_private_mode_type => FtaPrivateModeType.first)
     test_asset.secondary_fta_mode_types << FtaModeType.second
     test_asset.save!
     assign(:asset, test_asset)
     render
+    puts render
 
     expect(rendered).to have_content(FtaFundingType.first.to_s)
     expect(rendered).to have_content('22%')
