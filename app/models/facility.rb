608156861579a4bacc53c554cd0760c9fa25b79a
mathmerized <mathmerized@gmail.com>
2018-05-23 08:20:39 -0400

    move method_missing to link to old asset to highest acting_as level

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -6,4 +6,20 @@ class Facility < ApplicationRecord
   belongs_to :fta_private_mode
   belongs_to :land_owner_organization
   belongs_to :facility_owner_organization
+
+  protected
+
+  # link to old asset if no instance method in chain
+  def method_missing(method, *args, &block)
+    if !self_respond_to?(method) && acting_as.respond_to?(method)
+      acting_as.send(method, *args, &block)
+    elsif !self_respond_to?(method) && typed_asset.respond_to?(method)
+      puts "You are calling the old asset for this method"
+      Rails.logger.warn "You are calling the old asset for this method"
+      typed_asset.send(method, *args, &block)
+    else
+      super
+    end
+  end
+
 end
