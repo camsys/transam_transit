4aaac90ec9a33124728495218c2141a994882ee5
mathmerized <mathmerized@gmail.com>
2018-06-05 21:22:25 -0400

    transit system config extensions

---
+++ db/data_migrations/20180605195732_add_transit_system_config_extensions.rb
@@ -0,0 +1,11 @@
+class AddTransitSystemConfigExtensions < ActiveRecord::DataMigration
+  def up
+    system_config_extensions = [
+        {class_name: 'TransitAsset', extension_name: 'TransamKeywordSearchable', active: true}
+    ]
+
+    system_config_extensions.each do |extension|
+      SystemConfigExetnsion.create!(extension)
+    end
+  end
+end
