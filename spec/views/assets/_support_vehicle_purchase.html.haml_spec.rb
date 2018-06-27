3ab2c10b6849f9474edf4bcd989c31b95fb589e5
mathmerized <mathmerized@gmail.com>
2018-06-27 09:15:11 -0400

    fix tests - the test was written badly to take just the asset factory setting ESL miles but actually suppose to come from policy rule factory

--- spec/views/assets/_support_vehicle_purchase.html.haml_spec.rb
+++ spec/views/assets/_support_vehicle_purchase.html.haml_spec.rb
@@ -13,7 +13,7 @@ describe "assets/_support_vehicle_purchase.html.haml", :type => :view do
     expect(rendered).to have_content('03/01/2010')
     expect(rendered).to have_content(test_asset.vendor.to_s)
     expect(rendered).to have_content('12 yrs')
-    expect(rendered).to have_content('44,444')
+    expect(rendered).to have_content('500,000')
     expect(rendered).to have_content(test_asset.title_number)
     expect(rendered).to have_content(test_asset.title_owner.name)
   end
