383ef80d670897b14497eed73410fc33d613d803
Alex Andrade <aandrade@camsys.com>
2018-08-09 11:34:02 -0400

    Add table view models and start to populate views.

---
+++ app/models/capital_equipment_asset_table_view.rb
@@ -0,0 +1,63 @@
+class CapitalEquipmentAssetTableView  < ActiveRecord::Base
+
+  def readonly?
+    true
+  end
+
+  #These associations are to more quickly support the access of recent asset events for the model
+  belongs_to :capital_equipment
+  belongs_to :condition_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :maintenance_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :service_status_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :location_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :rebuild_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :disposition_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :mileage_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :operation_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :facility_operation_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :vehicle_use_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :storage_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :useage_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :maintenace_history_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :early_disposition_event, class_name: 'AssetEvent', :foreign_key => :id
+  belongs_to :early_replacement_status_event, class_name: 'AssetEvent', :foreign_key => :id
+
+
+  def self.get_default_table_headers()
+    ["Asset ID", "Organization", "Description", "Manufacturer", "Model", "Year", "Class", "Type", "Status", "ESL",
+     "Last Life Cycle Action", "Life Cycle Action Date"]
+  end
+
+  def self.get_all_table_headers()
+    ["Asset ID", "Organization", "Description", "Manufacturer", "Model", "Year", "Class", "Type", "Status", "ESL",
+     "Last Life Cycle Action", "Life Cycle Action Date", "External ID", "Subtype", "Quantity", "Quantity Type",
+     "Funding Program (largest %)", "Direct Capital Responsibility", "Capital Responsibility %", "Asset Group",
+     "Service Life - Current", "TERM Condition", "TERM Rating", "Date of Condition Assessment", "Replace / Rehab Policy (ESL",
+     "ESL - Adjusted", "Rebuild / Rehab Type", "Date of Rebuild / Rehab", "Location", "Current Book Value",
+     "Replacement Status", "Replacement Policy Year", "Replacement Actual Year", "Scheduled Replacement Cost"]
+  end
+
+  def get_most_recent_event
+
+    event_list = []
+    event_list << condition_event
+    event_list << maintenance_event
+    event_list << service_status_event
+    event_list << location_event
+    event_list << rebuild_event
+    event_list << disposition_event
+    event_list << mileage_event
+    event_list << operation_event
+    event_list << facility_operation_event
+    event_list << vehicle_use_event
+    event_list << storage_event
+    event_list << useage_event
+    event_list << maintenace_history_event
+    event_list << early_disposition_event
+    event_list << early_replacement_status_event
+
+    event_list.sort_by{|event| event[:updated_at]}.first
+
+  end
+
+end
