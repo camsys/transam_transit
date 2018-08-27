da7c6797c24c24e4c382775b11f3d88a275721b8
mathmerized <mathmerized@gmail.com>
2018-08-27 08:04:46 -0400

    TTPLAT-358 add indices to transam_asset reference columns

--- db/migrate/20180725130835_add_transam_asset_asset_habtm_tables.rb
+++ db/migrate/20180725130835_add_transam_asset_asset_habtm_tables.rb
@@ -1,8 +1,8 @@
 class AddTransamAssetAssetHabtmTables < ActiveRecord::Migration[5.2]
   def change
-    add_column :assets_fta_mode_types, :transam_asset_id, :integer, after: :asset_id
-    add_column :assets_fta_service_types, :transam_asset_id, :integer, after: :asset_id
-    add_column :assets_vehicle_features, :transam_asset_id, :integer, after: :asset_id
-    add_column :assets_facility_features, :transam_asset_id, :integer, after: :asset_id
+    add_reference :assets_fta_mode_types, :transam_asset, after: :asset_id
+    add_reference :assets_fta_service_types, :transam_asset, after: :asset_id
+    add_reference :assets_vehicle_features, :transam_asset, after: :asset_id
+    add_reference :assets_facility_features, :transam_asset, after: :asset_id
   end
 end
