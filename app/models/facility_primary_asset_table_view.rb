c0a4a54886146264f40ba7d2cccb301302953764
Alex Andrade <aandrade@camsys.com>
2018-08-29 14:03:35 -0400

    Complete Primary Facility Table View.

--- app/models/facility_primary_asset_table_view.rb
+++ app/models/facility_primary_asset_table_view.rb
@@ -21,7 +21,7 @@ class FacilityPrimaryAssetTableView  < ActiveRecord::Base
   def self.get_all_table_headers
     ["Asset ID", "Organization", "Facility Name", "Facility Categorization*", "Component - Sub-Component Type*",
      "Year", "Class", "Type", "Status", "ESL", "Last Life Cycle Action", "Life Cycle Action Date",
-     "External ID", "Subtype*", "Funding Program (largest%)", "Direct Capital Responsibility", "Description*",
+     "External ID", "Subtype*", "Funding Program (largest%)", "Cost (Purchase)", "In Service Date", "Direct Capital Responsibility", "Description*",
      "Asset Group", "Service Life - Current", "TERM Condition", "TERM Rating", "Date of Condition Assessment",
      "Replace / Rehab Policy (ESL)", "ESL - Adjusted", "Rebuild / Rehab Type", "Date of Rebuild / Rehab",
      "Location", "Current Book Value", "Replacement Status", "Replacement Policy Year",
