5c811fe68b12fc982f917846523c15a00ca4f231
Xudong Liu <xudong.camsys@gmail.com>
2018-09-06 16:04:13 -0400

    fix exporting underlying data for  asset service life summary report [TTPLAT-360]

--- app/reports/asset_service_life_report.rb
+++ app/reports/asset_service_life_report.rb
@@ -9,7 +9,6 @@ class AssetServiceLifeReport < AbstractReport
   COMMON_FORMATS_WITH_MILEAGE = [:string, :string, :integer, :integer, :percent, :integer, :percent, :integer, :percent]
 
   def self.get_underlying_data(organization_id_list, params)
-
     #asset_type_id = params[:asset_type_id].to_i > 0 ? params[:asset_type_id].to_i : 1 # default to rev vehicles
     #asset_type =  AssetType.find_by(id: asset_type_id)
 
@@ -33,56 +32,62 @@ class AssetServiceLifeReport < AbstractReport
                 .joins('INNER JOIN asset_types ON asset_subtypes.asset_type_id = asset_types.id')
                 .joins('INNER JOIN policies ON policies.organization_id = organizations.id')
                 .joins('LEFT JOIN (SELECT coalesce(SUM(extended_useful_life_months)) as sum_extended_eul, transam_asset_id FROM asset_events GROUP BY transam_asset_id) as rehab_events ON rehab_events.transam_asset_id = transam_assets.id')
-                .joins('INNER JOIN manufacturer_models ON transam_assets.manufacturer_model_id = manufacturer_models.id')
-                .joins('INNER JOIN recent_asset_events_views ON recent_asset_events_views.transam_asset_id = transam_assets.id')
-                .joins('INNER JOIN asset_events ON asset_events.id = recent_asset_events_views.asset_event_id')
+                .joins('LEFT JOIN manufacturer_models ON transam_assets.manufacturer_model_id = manufacturer_models.id')
+                .joins('LEFT JOIN recent_asset_events_views AS recent_milage ON recent_milage.transam_asset_id = transam_assets.id AND recent_milage.asset_event_name = "Mileage"')
+                .joins('LEFT JOIN asset_events AS mileage_event ON mileage_event.id = recent_milage.asset_event_id')
+                .joins('LEFT JOIN recent_asset_events_views AS recent_rating ON recent_rating.transam_asset_id = transam_assets.id AND recent_rating.asset_event_name = "Condition"')
+                .joins('LEFT JOIN asset_events AS rating_event ON rating_event.id = recent_rating.asset_event_id')
                 .where(organization_id: organization_id_list, fta_asset_category_id: fta_asset_category_id)
                 .where(policies: {active: true})
-
+    
     if typed_asset_class.include? 'Vehicle'
-      query = query.joins('INNER JOIN policy_asset_subtype_rules ON policy_asset_subtype_rules.policy_id = policies.id AND policy_asset_subtype_rules.asset_subtype_id = asset_subtypes.id AND policy_asset_subtype_rules.fuel_type_id = service_vehicles.fuel_type_id')
+      query = query.joins('INNER JOIN policy_asset_subtype_rules ON policy_asset_subtype_rules.policy_id = policies.id AND policy_asset_subtype_rules.asset_subtype_id = asset_subtypes.id AND policy_asset_subtype_rules.fuel_type_id = service_vehicles.fuel_type_id AND policy_asset_subtype_rules.default_rule = TRUE')
     else
-      query = query.joins('INNER JOIN policy_asset_subtype_rules ON policy_asset_subtype_rules.policy_id = policies.id AND policy_asset_subtype_rules.asset_subtype_id = asset_subtypes.id')
+      query = query.joins('INNER JOIN policy_asset_subtype_rules ON policy_asset_subtype_rules.policy_id = policies.id AND policy_asset_subtype_rules.asset_subtype_id = asset_subtypes.id AND policy_asset_subtype_rules.default_rule = TRUE')
     end
-
+    
     manufacturer_model = 'IF(manufacturer_models.name = "Other",transam_assets.other_manufacturer_model,manufacturer_models.name)'
-
+    
     if typed_asset_class.include? 'Vehicle'
       query = query
-                  .joins("INNER JOIN fuel_types ON service_vehicles.fuel_type_id = fuel_types.id")
-                  .joins("INNER JOIN manufacturers ON transam_assets.manufacturer_id = manufacturers.id")
-                  .joins("INNER JOIN serial_numbers ON transam_assets.id = serial_numbers.identifiable_id AND serial_numbers.identifiable_type = 'TransamAsset'")
+                  .joins("LEFT JOIN fuel_types ON service_vehicles.fuel_type_id = fuel_types.id")
+                  .joins("LEFT JOIN manufacturers ON transam_assets.manufacturer_id = manufacturers.id")
+                  .joins("LEFT JOIN serial_numbers ON transam_assets.id = serial_numbers.identifiable_id AND serial_numbers.identifiable_type = 'TransamAsset'")
+      
       if typed_asset_class == 'RevenueVehicle'
         query = query
-                    .joins('INNER JOIN fta_vehicle_types ON transit_assets.fta_asset_class_id = fta_vehicle_types.fta_asset_class_id')
+                    .joins('INNER JOIN fta_vehicle_types ON transit_assets.fta_type_id = fta_vehicle_types.id AND transit_assets.fta_type_type="FtaVehicleType"')
+        
         vehicle_type = 'fta_vehicle_types.name'
       else
-        query = query.joins('INNER JOIN fta_support_vehicle_types ON transit_assets.fta_asset_class_id = fta_support_vehicle_types.fta_asset_class_id')
+        query = query
+                    .joins('INNER JOIN fta_support_vehicle_types ON transit_assets.fta_type_id = fta_support_vehicle_types.id AND transit_assets.fta_type_type="FtaSupportVehicleType"')
+        
         vehicle_type = 'fta_support_vehicle_types.name'
       end
 
-      cols = ['organizations.short_name', 'asset_types.name', 'asset_subtypes.name', vehicle_type, 'transam_assets.asset_tag', 'transam_assets.external_id',	'serial_numbers.identification', 'service_vehicles.license_plate', 'transam_assets.manufacture_year', 'CONCAT(manufacturers.code,"-", manufacturers.name)', manufacturer_model, 'CONCAT(fuel_types.code,"-", fuel_types.name)', 'transam_assets.in_service_date', 'transam_assets.purchase_date', 'transam_assets.purchase_cost', 'IF(transam_assets.purchased_new, "YES", "NO")', 'IF(IFNULL(sum_extended_eul, 0)>0, "YES", "NO")',"YEAR('#{Date.today}')*12+MONTH('#{Date.today}')-YEAR(in_service_date)*12-MONTH(in_service_date)",'IF(transam_assets.purchased_new, policy_asset_subtype_rules.min_service_life_months,policy_asset_subtype_rules.min_used_purchase_service_life_months)+ IFNULL(sum_extended_eul, 0)', 'asset_events.current_mileage','policy_asset_subtype_rules.min_service_life_miles', 'asset_events.assessed_rating', 'policies.condition_threshold']
+      cols = ['organizations.short_name', 'asset_types.name', 'asset_subtypes.name', vehicle_type, 'transam_assets.asset_tag', 'transam_assets.external_id',  'serial_numbers.identification', 'service_vehicles.license_plate', 'transam_assets.manufacture_year', 'CONCAT(manufacturers.code,"-", manufacturers.name)', manufacturer_model, 'CONCAT(fuel_types.code,"-", fuel_types.name)', 'transam_assets.in_service_date', 'transam_assets.purchase_date', 'transam_assets.purchase_cost', 'IF(transam_assets.purchased_new, "YES", "NO")', 'IF(IFNULL(sum_extended_eul, 0)>0, "YES", "NO")',"YEAR('#{Date.today}')*12+MONTH('#{Date.today}')-YEAR(in_service_date)*12-MONTH(in_service_date)",'IF(transam_assets.purchased_new, policy_asset_subtype_rules.min_service_life_months,policy_asset_subtype_rules.min_used_purchase_service_life_months)+ IFNULL(sum_extended_eul, 0)', 'mileage_event.current_mileage','policy_asset_subtype_rules.min_service_life_miles', 'rating_event.assessed_rating', 'policies.condition_threshold']
 
-      labels =[ 'Agency','Asset Type','Asset Subtype',	'FTA Vehicle Type',	'Asset Tag',	'External ID',	'VIN','License Plate',	'Manufacturer Year', 	'Manufacturer',	'Model',	'Fuel Type',	'In Service Date', 'Purchase Date',	'Purchase Cost',	'Purchased New', 'Rehabbed Asset?',	'Current Age (mo.)', 	'Policy ESL (mo.)',	'Current Mileage (mi.)',	'Policy ESL (mi.)',	'Current Condition (TERM)',	'Policy Condition Threshold (TERM)']
+      labels =['Agency','Asset Type','Asset Subtype', 'FTA Vehicle Type', 'Asset Tag',  'External ID',  'VIN','License Plate',  'Manufacturer Year',  'Manufacturer', 'Model',  'Fuel Type',  'In Service Date', 'Purchase Date', 'Purchase Cost',  'Purchased New', 'Rehabbed Asset?', 'Current Age (mo.)',  'Policy ESL (mo.)', 'Current Mileage (mi.)',  'Policy ESL (mi.)', 'Current Condition (TERM)', 'Policy Condition Threshold (TERM)']
 
       formats = [:string, :string, :string, :string, :string, :string, :string, :string, :integer, :string, :string, :string, :date, :date, :currency, :string, :string, :integer, :integer, :integer, :integer, :decimal, :decimal]
     elsif typed_asset_class.include? 'Facility'
       query = query
-                  .joins('INNER JOIN fta_facility_types ON transit_assets.fta_asset_class_id = fta_facility_types.fta_asset_class_id')
+                  .joins('INNER JOIN fta_support_vehicle_types ON transit_assets.fta_type_id = fta_facility_types.id AND transit_assets.fta_type_type="FtaFacilityType"')
+        
+      cols = ['organizations.short_name', 'asset_types.name', 'asset_subtypes.name', 'fta_facility_types.name', 'transam_assets.asset_tag', 'transam_assets.external_id', 'facilities.facility_name', 'facilities.address1', 'facilities.address2', 'facilities.city', 'facilities.state','facilities.zip', 'transam_assets.manufacture_year', 'transam_assets.in_service_date', 'transam_assets.purchase_date', 'transam_assets.purchase_cost', 'IF(transam_assets.purchased_new, "YES", "NO")', 'IF(IFNULL(sum_extended_eul, 0)>0, "YES", "NO")',"YEAR('#{Date.today}')*12+MONTH('#{Date.today}')-YEAR(in_service_date)*12-MONTH(in_service_date)",'IF(transam_assets.purchased_new, policy_asset_subtype_rules.min_service_life_months,policy_asset_subtype_rules.min_used_purchase_service_life_months)+ IFNULL(sum_extended_eul, 0)', 'rating_event.assessed_rating', 'policies.condition_threshold']
 
-      cols = ['organizations.short_name', 'asset_types.name', 'asset_subtypes.name', 'fta_facility_types.name', 'transam_assets.asset_tag', 'transam_assets.external_id',	'facilities.facility_name', 'facilities.address1', 'facilities.address2', 'facilities.city', 'facilities.state','facilities.zip', 'transam_assets.manufacture_year', 'transam_assets.in_service_date', 'transam_assets.purchase_date', 'transam_assets.purchase_cost', 'IF(transam_assets.purchased_new, "YES", "NO")', 'IF(IFNULL(sum_extended_eul, 0)>0, "YES", "NO")',"YEAR('#{Date.today}')*12+MONTH('#{Date.today}')-YEAR(in_service_date)*12-MONTH(in_service_date)",'IF(transam_assets.purchased_new, policy_asset_subtype_rules.min_service_life_months,policy_asset_subtype_rules.min_used_purchase_service_life_months)+ IFNULL(sum_extended_eul, 0)', 'asset_events.assessed_rating', 'policies.condition_threshold']
-
-      labels =['Agency', 'Asset Type', 'Asset Subtype',	'FTA Facility Type', 'Asset Tag', 'External ID', 'Name', 'Address1', 'Address2', 'City', 'State', 'Zip', 'Year Built','In Service Date', 'Purchase Date',	'Purchase Cost',	'Purchased New', 'Rehabbed Asset?',	'Current Age (mo.)', 	'Policy ESL (mo.)',	'Current Condition (TERM)',	'Policy Condition Threshold (TERM)']
+      labels =['Agency', 'Asset Type', 'Asset Subtype', 'FTA Facility Type', 'Asset Tag', 'External ID', 'Name', 'Address1', 'Address2', 'City', 'State', 'Zip', 'Year Built','In Service Date', 'Purchase Date', 'Purchase Cost',  'Purchased New', 'Rehabbed Asset?', 'Current Age (mo.)',  'Policy ESL (mo.)', 'Current Condition (TERM)', 'Policy Condition Threshold (TERM)']
 
       formats = [:string, :string, :string, :string, :string, :string, :string, :string, :string, :string, :string, :string, :integer, :date, :date, :currency, :string, :string, :integer, :integer, :decimal, :decimal]
     else
       query = query
-                  .joins("INNER JOIN manufacturers ON transam_assets.manufacturer_id = manufacturers.id")
-                  .joins("INNER JOIN serial_numbers ON transam_assets.id = serial_numbers.identifiable_id AND serial_numbers.identifiable_type = 'TransamAsset'")
+                  .joins("LEFT JOIN manufacturers ON transam_assets.manufacturer_id = manufacturers.id")
+                  .joins("LEFT JOIN serial_numbers ON transam_assets.id = serial_numbers.identifiable_id AND serial_numbers.identifiable_type = 'TransamAsset'")
 
-      cols = ['organizations.short_name', 'asset_types.name', 'asset_subtypes.name', 'transam_assets.asset_tag', 'transam_assets.external_id',	'transam_assets.description', 'serial_numbers.identification', 'transam_assets.quantity', 'transam_assets.quantity_unit', 'transam_assets.manufacture_year', 'CONCAT(manufacturers.code,"-", manufacturers.name)', manufacturer_model, 'transam_assets.in_service_date', 'transam_assets.purchase_date', 'transam_assets.purchase_cost', 'IF(transam_assets.purchased_new, "YES", "NO")', 'IF(IFNULL(sum_extended_eul, 0)>0, "YES", "NO")',"YEAR('#{Date.today}')*12+MONTH('#{Date.today}')-YEAR(in_service_date)*12-MONTH(in_service_date)",'IF(transam_assets.purchased_new, policy_asset_subtype_rules.min_service_life_months,policy_asset_subtype_rules.min_used_purchase_service_life_months)+ IFNULL(sum_extended_eul, 0)', 'asset_events.current_mileage','policy_asset_subtype_rules.min_service_life_miles', 'asset_events.assessed_rating', 'policies.condition_threshold']
+      cols = ['organizations.short_name', 'asset_types.name', 'asset_subtypes.name', 'transam_assets.asset_tag', 'transam_assets.external_id',  'transam_assets.description', 'serial_numbers.identification', 'transam_assets.quantity', 'transam_assets.quantity_unit', 'transam_assets.manufacture_year', 'CONCAT(manufacturers.code,"-", manufacturers.name)', manufacturer_model, 'transam_assets.in_service_date', 'transam_assets.purchase_date', 'transam_assets.purchase_cost', 'IF(transam_assets.purchased_new, "YES", "NO")', 'IF(IFNULL(sum_extended_eul, 0)>0, "YES", "NO")',"YEAR('#{Date.today}')*12+MONTH('#{Date.today}')-YEAR(in_service_date)*12-MONTH(in_service_date)",'IF(transam_assets.purchased_new, policy_asset_subtype_rules.min_service_life_months,policy_asset_subtype_rules.min_used_purchase_service_life_months)+ IFNULL(sum_extended_eul, 0)', 'mileage_event.current_mileage','policy_asset_subtype_rules.min_service_life_miles', 'rating_event.assessed_rating', 'policies.condition_threshold']
 
-      labels =[ 'Agency','Asset Type','Asset Subtype',	'Asset Tag',	'External ID', 'Name',	'Serial Number',	'Quantity','Quantity Unit','Manufacturer Year', 	'Manufacturer',	'Model',	'In Service Date', 'Purchase Date',	'Purchase Cost',	'Purchased New', 'Rehabbed Asset?',	'Current Age (mo.)', 	'Policy ESL (mo.)',	'Current Mileage (mi.)',	'Policy ESL (mi.)',	'Current Condition (TERM)',	'Policy Condition Threshold (TERM)']
+      labels =[ 'Agency','Asset Type','Asset Subtype',  'Asset Tag',  'External ID', 'Name',  'Serial Number',  'Quantity','Quantity Unit','Manufacturer Year',   'Manufacturer', 'Model',  'In Service Date', 'Purchase Date', 'Purchase Cost',  'Purchased New', 'Rehabbed Asset?', 'Current Age (mo.)',  'Policy ESL (mo.)', 'Current Mileage (mi.)',  'Policy ESL (mi.)', 'Current Condition (TERM)', 'Policy Condition Threshold (TERM)']
 
       formats = [:string, :string, :string, :string, :string, :string, :string, :integer, :string, :integer, :string, :string, :date, :date, :currency, :string, :string, :integer, :integer, :integer, :integer, :decimal, :decimal]
 
