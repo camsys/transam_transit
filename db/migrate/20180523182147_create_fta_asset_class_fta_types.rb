a15f48410e3038e733502dff7d5c3df998a3d061
mathmerized <mathmerized@gmail.com>
2018-09-27 13:57:30 -0400

    Bump version and rename data migrations so theyre not the same class names as schema migrations, try again

--- db/migrate/20180523182147_create_fta_asset_class_fta_types.rb
+++ db/migrate/20180523182147_create_fta_asset_class_fta_types.rb
@@ -1,4 +1,4 @@
-class AddFtaAssetClassFtaTypes < ActiveRecord::Migration[5.2]
+class CreateFtaAssetClassFtaTypes < ActiveRecord::Migration[5.2]
   def change
     add_column :fta_vehicle_types, :fta_asset_class_id, :integer, after: :id
     add_column :fta_facility_types, :fta_asset_class_id, :integer, after: :id
