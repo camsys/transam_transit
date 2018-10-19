c41b6f757ad687421f7218301e5571ce4939a3e1
mathmerized <mathmerized@gmail.com>
2018-10-19 15:32:35 -0400

    TTPLAT-481 fix bad data load of getting locations from Asset to TransamAsset

---
+++ db/data_migrations/20181019163553_fix_data_load_location_id_transam_assets.rb
@@ -0,0 +1,12 @@
+class FixDataLoadLocationIdTransamAssets < ActiveRecord::DataMigration
+  def up
+    TransitAsset.where.not(transam_assets: {location_id: nil}, transit_assets: {asset_id: nil}).each do |transit_asset|
+      old_asset = transit_asset.asset
+
+      if transit_asset.location_id == transit_asset.asset.location_id
+        transit_asset.location_id = TransitAsset.find_by(asset_id: transit_asset.asset.location_id).transam_asset.id
+        transit_asset.save(validate: false)
+      end
+    end
+  end
+end
