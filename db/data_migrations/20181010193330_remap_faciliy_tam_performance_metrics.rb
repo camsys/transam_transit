cfa1f32b2252453dc4c9c6c75b3877dd4dac13a5
mathmerized <mathmerized@gmail.com>
2018-10-24 15:03:50 -0400

    bug fixes - performance restriction add link to track, fix column name in migration

--- db/data_migrations/20181010193330_remap_faciliy_tam_performance_metrics.rb
+++ db/data_migrations/20181010193330_remap_faciliy_tam_performance_metrics.rb
@@ -5,11 +5,11 @@ class RemapFaciliyTamPerformanceMetrics < ActiveRecord::DataMigration
         old_metrics = group.tam_performance_metrics.where(asset_level: FtaFacilityType.where(fta_asset_class: fta_asset_class))
         if old_metrics.count > 0
           new_metric = old_metrics.first
-          if old_metrics.distinct.pluck(:useful_life_benchmark, :goal_pcnt).count == 1
+          if old_metrics.distinct.pluck(:useful_life_benchmark, :pcnt_goal).count == 1
             new_metric.update(asset_level: fta_asset_class)
           else
             puts "Tam Group #{group}: #{fta_asset_class} defaulted to 3.0 TERM and max goal pcnt"
-            new_metric.update(asset_level: fta_asset_class, useful_life_benchmark: 3, goal_pcnt: old_metrics.maximum(:goal_pcnt))
+            new_metric.update(asset_level: fta_asset_class, useful_life_benchmark: 3, pcnt_goal: old_metrics.maximum(:pcnt_goal))
           end
           old_metrics.where.not(object_key: new_metric.object_key).delete_all
         end
