7dfbe085e0d8205a0c6c9374ca1bab80ec2507fe
mathmerized <mathmerized@gmail.com>
2018-11-01 14:20:54 -0400

    TTPLAT-497 fix data load of fixing modes and service types

--- db/data_migrations/20181026164206_fix_data_load_fta_mode_service_transam_assets.rb
+++ db/data_migrations/20181026164206_fix_data_load_fta_mode_service_transam_assets.rb
@@ -2,8 +2,9 @@ class FixDataLoadFtaModeServiceTransamAssets < ActiveRecord::DataMigration
   def up
     if Infrastructure.count == 0 # no client has infrastructure except sfrta
 
-      Facility.where(transit_assets: {asset_id: AssetsFtaModeType.select(:asset_id)}).update_all(transam_asset_type: 'Facility')
-      ServiceVehicle.where(transit_assets: {asset_id: AssetsFtaModeType.select(:asset_id)}).update_all(transam_asset_type: 'ServiceVehicle')
+      AssetsFtaModeType.where(asset_id: Facility.pluck(:asset_id)).update_all(transam_asset_type: 'Facility')
+      AssetsFtaModeType.where(asset_id: ServiceVehicle.pluck(:asset_id)).update_all(transam_asset_type: 'ServiceVehicle')
+
       if Facility.where(id: AssetsFtaModeType.where(asset_id: nil).select(:transam_asset_id)).count == 0 # make sure all new assets are vehicles
         AssetsFtaModeType.where(asset_id: nil).update_all(transam_asset_type: 'ServiceVehicle')
       end
