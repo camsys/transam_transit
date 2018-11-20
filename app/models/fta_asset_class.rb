7d527e8bed48dd4d0fa3086006716cc8a0f01215
mathmerized <mathmerized@gmail.com>
2018-11-20 10:22:10 -0500

    use named arguments for getting class from fta asset class

--- app/models/fta_asset_class.rb
+++ app/models/fta_asset_class.rb
@@ -10,17 +10,17 @@ class FtaAssetClass < ApplicationRecord
   end
 
   # override column in database as other
-  def class_name(opts: {},asset: nil)
+  def class_name(opts: {},assets: nil)
     if fta_asset_category.name == 'Facilities'
       if opts[:is_component].to_i != Facility::CATEGORIZATION_PRIMARY
         'FacilityComponent'
-      elsif asset.present? && (asset.very_specific.class.to_s.include? 'Component')
+      elsif assets.present? && (assets.very_specific.class.to_s.include? 'Component')
         'FacilityComponent'
       else
         read_attribute(:class_name)
       end
     elsif fta_asset_category.name == 'Infrastructure'
-      if asset.present? && (asset.very_specific.class.to_s.include? 'Component')
+      if assets.present? && (assets.very_specific.class.to_s.include? 'Component')
         'InfrastructureComponent'
       else
         read_attribute(:class_name)
