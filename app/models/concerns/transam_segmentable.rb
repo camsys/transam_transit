3c388b3c70ee508b22587271e2e7811cdb77b93c
mathmerized <mathmerized@gmail.com>
2018-12-05 11:09:12 -0500

    TTPLAT-567 fix overlaps check again. point is a never ending line not a point that has to be in a segment

--- app/models/concerns/transam_segmentable.rb
+++ app/models/concerns/transam_segmentable.rb
@@ -88,9 +88,9 @@ module TransamSegmentable
   def overlaps(instance)
     if (instance.class < TransamSegmentable).present?
       if segment.present?
-        instance.segment.present? ? segment_without_ends.overlaps?(instance.segment_without_ends) : segment.member?(instance.point)
+        instance.segment.present? ? segment_without_ends.overlaps?(instance.segment_without_ends) : instance.point <= segment.min &&  segment.min <= segment.max
       else
-        instance.segment.present? ? instance.segment.member?(point) : true # a point indicates a starting point for a never ending segment, two points always overlap
+        instance.segment.present? ? point <= instance.segment.min && instance.segment.min <= instance.segment.max : true # a point indicates a starting point for a never ending segment, two points always overlap
       end
     else
       false
