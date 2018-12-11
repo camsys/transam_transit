02444ef22efeee6b1fe2d1c1e1477f0d3e4002f5
mathmerized <mathmerized@gmail.com>
2018-12-11 13:59:16 -0500

    fix indices again

--- app/file_handlers/a90_template_builder.rb
+++ app/file_handlers/a90_template_builder.rb
@@ -19,9 +19,11 @@ class A90TemplateBuilder < TemplateBuilder
   # Add a row for each of the asset for the org
   def add_rows(sheet)
 
-    idx = 3
+    idx = 5
+    sheet.add_row
+    sheet.add_row
     sheet.add_row ['1. Revenue Vehicles - Percent of revenue vehicles that have met or exceeded their useful life benchmark']
-    sheet.merge_cells "A#{idx}:F#{idx}"
+    #sheet.merge_cells "A#{idx}:F#{idx}"
     sheet.add_row subheader_row
 
     FtaVehicleType.active.each do |fta_vehicle_type|
@@ -32,8 +34,9 @@ class A90TemplateBuilder < TemplateBuilder
     end
 
     idx += 1
+    sheet.add_row
     sheet.add_row ['2. Service Vehicles - Percent of service vehicles that have met or exceeded their useful life benchmark']
-    sheet.merge_cells "A#{idx}:F#{idx}"
+    #sheet.merge_cells "A#{idx}:F#{idx}"
     sheet.add_row subheader_row
     FtaSupportVehicleType.active.each do |fta_vehicle_type|
       ntd_performance_measure = NtdPerformanceMeasure.find_by(fta_asset_category: FtaAssetCategory.find_by(name: 'Equipment'), asset_level: fta_vehicle_type.name)
@@ -44,8 +47,9 @@ class A90TemplateBuilder < TemplateBuilder
 
     idx += 1
     fta_asset_category = FtaAssetCategory.find_by(name: 'Facilities')
+    sheet.add_row
     sheet.add_row ['3. Facility - Percent of facilities rated 3 or below on the condition scale']
-    sheet.merge_cells "A#{idx}:F#{idx}"
+    #sheet.merge_cells "A#{idx}:F#{idx}"
     sheet.add_row subheader_row
     FtaAssetClass.where(fta_asset_category: fta_asset_category).active.each do |fta_class|
       ntd_performance_measure = NtdPerformanceMeasure.find_by(fta_asset_category: fta_asset_category, asset_level: fta_class.name)
@@ -55,8 +59,9 @@ class A90TemplateBuilder < TemplateBuilder
     end
 
     idx += 1
+    sheet.add_row
     sheet.add_row ['4. Infrastructure - Percent of track segments with performance restrictions']
-    sheet.merge_cells "A#{idx}:F#{idx}"
+    #sheet.merge_cells "A#{idx}:F#{idx}"
     sheet.add_row subheader_row
     FtaModeType.active.each do |fta_mode_type|
       ntd_performance_measure = NtdPerformanceMeasure.find_by(fta_asset_category: FtaAssetCategory.find_by(name: 'Infrastructure'), asset_level: fta_mode_type.to_s)
@@ -79,12 +84,12 @@ class A90TemplateBuilder < TemplateBuilder
   end
 
   def row_styles
-    styles = [
-      {:name => 'gray', :row => 0},
-      {:name => 'lt-gray', :row => 1}
-    ]
+    styles = []
+    idx = 2
+    styles << {:name => 'gray', :row => idx}
 
-    idx = 1
+    idx +=1
+    idx << {:name => 'lt-gray', :row => idx}
 
     idx += FtaVehicleType.active.count + 1
     styles << {:name => 'gray', :row => idx}
