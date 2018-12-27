08fe44fa1dec4bd36b3529463f1828bf2ccbc8e8
Xudong Liu <xudong.camsys@gmail.com>
2018-12-27 17:04:55 -0500

    TTPLAT-12 add db view for fta_type filter

--- db/seeds/asset_query_seeds.rb
+++ db/seeds/asset_query_seeds.rb
@@ -1,16 +1,16 @@
 ### Load asset query configurations
 puts "======= Loading transit asset query configurations ======="
 
-# Query Asset Classes
-asset_table = QueryAssetClass.find_or_create_by(table_name: 'transit_assets', transam_assets_join: "LEFT JOIN transit_assets ON transit_assets.id = transam_assets.transam_assetible_id and transam_assets.transam_assetible_type = 'TransitAsset'")
-
+# transit_assets table
+QueryAssetClass.find_or_create_by(table_name: 'transit_assets', transam_assets_join: "LEFT JOIN transit_assets ON transit_assets.id = transam_assets.transam_assetible_id and transam_assets.transam_assetible_type = 'TransitAsset'")
 # Query Category and fields
-category_fields = {
+transit_assets_category_fields = {
   'Identification & Classification': [
     {
       name: 'fta_asset_category_id',
       label: 'Category',
       filter_type: 'multi_select',
+      auto_show: show,
       association: {
         table_name: 'fta_asset_categories',
         display_field_name: 'name'
@@ -24,11 +24,6 @@ category_fields = {
         table_name: 'fta_asset_classes',
         display_field_name: 'name'
       }
-    },
-    {
-      name: 'fta_type_type',
-      label: 'Type',
-      filter_type: 'multi_select'
     }
   ],
 
@@ -126,22 +121,56 @@ category_fields = {
   ]
 }
 
-category_fields.each do |category_name, fields|
-  qc = QueryCategory.find_or_create_by(name: category_name)
-  fields.each do |field|
-    if field[:association]
-      qac = QueryAssociationClass.find_or_create_by(field[:association])
+# transit_asset_types view
+# create the view
+transit_asset_types_view_sql = <<-SQL
+  CREATE OR REPLACE VIEW transit_asset_types_view AS
+    SELECT ta.id, concat(ta.fta_type_type, '_', ta.fta_type_id) as fta_type_type_id,concat(COALESCE(fvt.name, ''), COALESCE(fsvt.name, ''), COALESCE(fft.name, ''), COALESCE(fet.name, '')) as fta_type FROM transit_assets as ta 
+    left join fta_vehicle_types as fvt on fvt.id = ta.fta_type_id and ta.fta_type_type = 'FtaVehicleType'
+    left join fta_support_vehicle_types as fsvt on fsvt.id = ta.fta_type_id and ta.fta_type_type = 'FtaSupportVehicleType'
+    left join fta_facility_types as fft on fft.id = ta.fta_type_id and ta.fta_type_type = 'FtaFacilityType'
+    left join fta_equipment_types as fet on fet.id = ta.fta_type_id and ta.fta_type_type = 'FtaEquipmentType';
+SQL
+ActiveRecord::Base.connection.execute transit_asset_types_view_sql
+
+QueryAssetClass.find_or_create_by(table_name: 'transit_asset_types_view', transam_assets_join: "LEFT JOIN transit_asset_types_view ON transit_asset_types_view.id = transam_assets.transam_assetible_id and transam_assets.transam_assetible_type = 'TransitAsset'")
+# Query Category and fields
+transit_asset_types_category_fields = {
+  'Identification & Classification': [
+    {
+      name: 'fta_type_type_id', # used to query
+      label: 'Type',
+      display_field: 'fta_type', # used to display
+      filter_type: 'multi_select'
+    }
+  ]
+}
+
+# seeding
+field_data = {
+  transit_assets: transit_assets_category_fields,
+  transit_asset_types_view: transit_asset_types_category_fields
+}
+field_data.each do |table_name, category_fields|
+  asset_table = QueryAssetClass.find_by_table_name table_name
+  category_fields.each do |category_name, fields|
+    qc = QueryCategory.find_or_create_by(name: category_name)
+    fields.each do |field|
+      if field[:association]
+        qac = QueryAssociationClass.find_or_create_by(field[:association])
+      end
+      qf = QueryField.find_or_create_by(
+        name: field[:name], 
+        label: field[:label], 
+        query_category: qc, 
+        query_association_class_id: qac.try(:id),
+        filter_type: field[:filter_type],
+        auto_show: field[:auto_show],
+        hidden: field[:hidden],
+        pairs_with: field[:pairs_with]
+      )
+      qf.query_asset_classes << asset_table
     end
-    qf = QueryField.find_or_create_by(
-      name: field[:name], 
-      label: field[:label], 
-      query_category: qc, 
-      query_association_class_id: qac.try(:id),
-      filter_type: field[:filter_type],
-      hidden: field[:hidden],
-      pairs_with: field[:pairs_with]
-    )
-    qf.query_asset_classes << asset_table
   end
 end
 
