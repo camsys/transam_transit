c56b7c131db5e4f8cad4946a25150225119781da
mathmerized <mathmerized@gmail.com>
2018-12-27 14:44:58 -0500

    TTPLAT-497 TTPLAT-622 fix associations on FTA modes and service types where new polymorphic association doesn't work

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -18,7 +18,7 @@ class Facility < TransamAssetRecord
   # Each facility has a set (0 or more) of fta mode type. This is the primary mode
   # serviced at the facility
   has_many                  :assets_fta_mode_types,       :as => :transam_asset,    :join_table => :assets_fta_mode_types
-  has_and_belongs_to_many   :fta_mode_types,              :as => :transam_asset,    :join_table => :assets_fta_mode_types
+  has_many                  :fta_mode_types,           :through => :assets_fta_mode_types
 
   # These associations support the separation of mode types into primary and secondary.
   has_one :primary_assets_fta_mode_type, -> { is_primary },
