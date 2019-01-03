deea5f6666ac9fc902c6522462fbed3971f5e9d0
mathmerized <mathmerized@gmail.com>
2019-01-03 13:56:04 -0500

    TTPLAT-624 fix bad mapping to TransamAsset and setting of fta_type for maintenance and communications equipment

---
+++ db/data_migrations/20190103184334_fixes_move_assets_transam_assets.rb
@@ -0,0 +1,10 @@
+class FixesMoveAssetsTransamAssets < ActiveRecord::DataMigration
+  def up
+    # fix bad mapping of old Asset to TransamAsset of fta_type for Maintenance Equipment
+    TransitAsset.where(asset: Asset.where(asset_subtype: AssetSubtype.where(asset_type: AssetType.find_by(name: 'Maintenance Equipment')).where.not(name: 'Fare Collection Systems'))).update_columns(fta_type_type: 'FtaEquipmentType', fta_type_id: FtaEquipmentType.find_by(name: 'Maintenance Equipment').id)
+
+    # fix bad mapping of old Asset to TransamAsset of fta_type for Communications Equipment
+    TransitAsset.where(asset: Asset.where(asset_subtype: AssetSubtype.where(asset_type: AssetType.find_by(name: 'Communications Equipment')).where.not(name: 'Surveillance & Security'))).update_columns(fta_type_type: 'FtaEquipmentType', fta_type_id: FtaEquipmentType.find_by(name: 'Communications Equipment').id)
+
+  end
+end
