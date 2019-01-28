06ab08441d594c2da8733d14f21697240179acd3
mathmerized <mathmerized@gmail.com>
2019-01-28 14:06:07 -0500

    fix tests for moving AssetMapSearcher to spatial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1319,15 +1319,15 @@ manufacturers = manufacturers.flatten
 
 system_config_extensions = [
     {class_name: 'TransamAsset', extension_name: 'PolicyAware', active: true},
-    {class_name: 'TransamAsset', extension_name: 'ReplaceableAsset', active: true},
-    {class_name: 'AssetMapSearcher', extension_name: 'TransitAssetMapSearchable', active: true}
+    {class_name: 'TransamAsset', extension_name: 'ReplaceableAsset', active: true}
 ]
 if SystemConfig.transam_module_loaded? :spatial
     system_config_extensions += [
         {class_name: 'Facility', extension_name: 'TransamAddressLocatable', active: true},
         {class_name: 'ServiceVehicle', extension_name: 'TransamParentLocatable', active: true},
         {class_name: 'CapitalEquipment', extension_name: 'TransamParentLocatable', active: true},
-        {class_name: 'Infrastructure', extension_name: 'TransamCoordinateLocatable', active: true}
+        {class_name: 'Infrastructure', extension_name: 'TransamCoordinateLocatable', active: true},
+        {class_name: 'AssetMapSearcher', extension_name: 'TransitAssetMapSearchable', active: true}
     ]
 end
 
