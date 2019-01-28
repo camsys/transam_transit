3c546b1849ac4c762fa5471b16087cabe8b43488
mathmerized <mathmerized@gmail.com>
2019-01-28 13:41:17 -0500

    TTPLAT-667 TTPLAT-685 fix bad refactor where you check if component. very_specific might already be cached highest class or class pulled from DB table name so you just need to check if class contains Component not the whole class name

--- app/models/fta_asset_class.rb
+++ app/models/fta_asset_class.rb
@@ -14,13 +14,13 @@ class FtaAssetClass < ApplicationRecord
     if fta_asset_category.name == 'Facilities'
       if opts[:is_component].to_i != Facility::CATEGORIZATION_PRIMARY
         'FacilityComponent'
-      elsif assets.present? && (assets.very_specific.class.to_s.include? 'TransitComponent')
+      elsif assets.present? && (assets.very_specific.class.to_s.include? 'Component') # asset.very_specific class might be already highest class since cached not just the class as determined from DB table name so check if contains Component and can be either FacilityComponent or TransitComponent
         'FacilityComponent'
       else
         read_attribute(:class_name)
       end
     elsif fta_asset_category.name == 'Infrastructure'
-      if assets.present? && (assets.very_specific.class.to_s.include? 'TransitComponent')
+      if assets.present? && (assets.very_specific.class.to_s.include? 'Component')
         'InfrastructureComponent'
       else
         read_attribute(:class_name)
