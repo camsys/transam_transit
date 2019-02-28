b6e794893e9a58c44b84d6e8eca63c59bc3eb207
mathmerized <mathmerized@gmail.com>
2019-02-28 13:26:02 -0500

    TTPLAT-642 cache inventory table results and prev/next arrows on detail page

--- app/decorators/assets_controller_decorator.rb
+++ app/decorators/assets_controller_decorator.rb
@@ -131,11 +131,9 @@ AssetsController.class_eval do
 
     end
 
-    if sorting_string.nil?
-      @assets.order(sorting_string.to_s).limit(params[:limit]).offset(params[:offset]).as_json(user: current_user, include_early_disposition: @early_disposition)
-    else
-      @assets.order(sorting_string.to_s).limit(params[:limit]).offset(params[:offset]).as_json(user: current_user, include_early_disposition: @early_disposition)
-    end
+    cache_list(@assets.order(sorting_string.to_s), AssetsController::INDEX_KEY_LIST_VAR)
+
+    @assets.order(sorting_string.to_s).limit(params[:limit]).offset(params[:offset]).as_json(user: current_user, include_early_disposition: @early_disposition)
 
   end
 
