0154357e0696fe3b00d96ad208d72d7d486795db
mathmerized <mathmerized@gmail.com>
2019-03-03 21:10:19 -0500

    rescue all errors (instead of specific) on rails.application.config that depend on DB

--- config/initializers/transam.rb
+++ config/initializers/transam.rb
@@ -12,7 +12,7 @@ Rails.application.config.rails_admin_transit_models = ['District', 'TamPolicy']
 # temporarily set a config on which assets to audit
 begin
   Rails.application.config.asset_auditor_config = {class_name: 'TransitAsset', query: {fta_asset_category: FtaAssetCategory.where.not(name: 'Infrastructure').ids}}
-rescue ActiveRecord::NoDatabaseError
-  puts "no database so not loading Rails.application.config that depends on DB"
+rescue
+  puts "skipped loading Rails.application.config that depends on DB"
 end
 
