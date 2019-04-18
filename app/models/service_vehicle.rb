0ae3398097c76a6276b2c96d291d5ac3adf305db
mathmerized <mathmerized@gmail.com>
2019-04-18 09:45:12 -0400

    fix gettin latest mileage to not include new records

--- app/models/service_vehicle.rb
+++ app/models/service_vehicle.rb
@@ -146,11 +146,11 @@ class ServiceVehicle < TransamAssetRecord
   end
 
   def reported_mileage
-    mileage_updates.last.try(:current_mileage)
+    mileage_updates.where.not(id: nil).last.try(:current_mileage)
   end
   
   def reported_mileage_date
-    mileage_updates.last.try(:event_date)
+    mileage_updates.where.not(id: nil).last.try(:event_date)
   end
 
   def fiscal_year_mileage(fy_year=nil)
