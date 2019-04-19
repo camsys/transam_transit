3a110ba49f65075dbe2095d87148375db1810d6b
mathmerized <mathmerized@gmail.com>
2019-04-19 08:46:14 -0400

    TTPLAT-896 fix checking policy rules if fuel type changed

--- app/models/service_vehicle.rb
+++ app/models/service_vehicle.rb
@@ -8,6 +8,14 @@ class ServiceVehicle < TransamAssetRecord
   after_save :check_fleet
   before_validation :cleanup_others
 
+  # check policy
+  after_save do
+    if saved_change_to_attribute?(:fuel_type_id) && !saved_change_to_attribute?(:asset_subtype_id)
+      transam_asset.send(:check_policy_rule)
+      transam_asset.send(:update_asset_state)
+    end
+  end
+
   belongs_to :chassis
   belongs_to :fuel_type
   belongs_to :dual_fuel_type
