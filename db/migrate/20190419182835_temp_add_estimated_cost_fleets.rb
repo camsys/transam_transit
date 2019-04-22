e92a30f2993975e7a9708bb014642e528e29b337
mathmerized <mathmerized@gmail.com>
2019-04-22 09:02:32 -0400

    TTPLAT-531 remove fleet columns now methods (again)

--- db/migrate/20190419182835_temp_add_estimated_cost_fleets.rb
+++
@@ -1,6 +0,0 @@
-class TempAddEstimatedCostFleets < ActiveRecord::Migration[5.2]
-  def change
-    add_column :asset_fleets, :estimated_cost, :integer
-    add_column :asset_fleets, :year_estimated_cost, :integer
-  end
-end
