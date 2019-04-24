287b6062ffb74db6f45aa3088c1c27f86ac9abd7
mathmerized <mathmerized@gmail.com>
2019-04-24 08:30:11 -0400

    TTPLAT-529 re-add back in db columns previously removed

--- db/migrate/20190424122722_add_parking_ntd_facilities.rb
+++ db/migrate/20190424122722_add_parking_ntd_facilities.rb
@@ -6,7 +6,7 @@ class AddParkingNtdFacilities < ActiveRecord::Migration[5.2]
 
     # The unit here is either square feet or parking space. This may be an overcomplication will need to check with Jack/Andreas
     unless column_exists? :ntd_facilities, :parking_measurement_unit
-      add_column :ntd_facilities, :parking_measurement_unit, :integer
+      add_column :ntd_facilities, :parking_measurement_unit, :string
     end
   end
 end
