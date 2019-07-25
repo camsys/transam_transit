956a2d56e2447e0d24b36e73b9f6ea7e22fa771f
mathmerized <mathmerized@gmail.com>
2019-07-25 16:53:43 -0400

    TTPLAT-777 add message templates to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1350,6 +1350,12 @@ manufacturers << locomotives
 manufacturers << support_vehicles
 manufacturers = manufacturers.flatten
 
+message_templates = [
+    {name: 'TamPolicy1', delivery_rules: 'When TAM group generated',subject: "TAM Group Generated", body: "The TAM Group: {tam_group}, has been generated for {tam_policy}. You have been designated as the group lead. You must assign metrics for the group, based on asset category and asset class/type/mode. Upon completion, you must distribute group metrics. You can access the group {link(here)}.", active: true, is_implemented: true},
+    {name: 'TamPolicy2', delivery_rules: 'When TAM group distributed', subject: "TAM Group Distributed", body: "The TAM Group: {tam_group}, has been distributed for {tam_policy}. The TAM Group: {tam_group}, has been created in your TAM policy, performance measures section. If you are able to make changes to the performance measures, you may make any changes needed, and activate the performance measures. If you are not allowed to make changes, the performance measures will be activated automatically. You can access the performance measures {link(here)}.", active: true, is_implemented: true},
+    {name: 'TamPolicy3', delivery_rules: 'When TAM group activated', subject: "TAM Performance Measures Activated",  body: "{organization} has activated the TAM performance measures, associated with the TAM Group: {tam_group} for {tam_policy}.You can access the {organization} performance measures {link(here)}.", active: true, is_implemented: true}
+]
+
 activities = [
     { name: 'Service Life Updates',
       description: 'Update policy replacement year for meeting mileage, condition policy rules.',
@@ -1383,7 +1389,7 @@ if SystemConfig.transam_module_loaded? :audit
   end
 end
 
-merge_tables = %w{ activities rule_sets roles asset_event_types condition_estimation_types service_life_calculation_types report_types manufacturers forms system_config_extensions }
+merge_tables = %w{ activities rule_sets roles asset_event_types condition_estimation_types service_life_calculation_types report_types manufacturers message_templates forms system_config_extensions }
 
 merge_tables.each do |table_name|
   puts "  Merging #{table_name}"
