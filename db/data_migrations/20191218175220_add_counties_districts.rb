faf981b5582cb3fe91acba2aa419562522a4ac71
mathmerized <mathmerized@gmail.com>
2019-12-27 14:18:43 -0500

    fix syntax error

--- db/data_migrations/20191218175220_add_counties_districts.rb
+++ db/data_migrations/20191218175220_add_counties_districts.rb
@@ -7,7 +7,7 @@ class AddCountiesDistricts < ActiveRecord::DataMigration
     require TransamTransit::Engine.root.join('db', 'seeds', 'districts_counties_seeds.rb')
 
     new_dists = District.where(district_type: DistrictType.find_by(name: 'County')).pluck(:id) - old_county_districts
-    old_county_districts = (old_county_districts - new_dists.map{|x| x.id})
+    old_county_districts = (old_county_districts - new_dists)
     if old_county_districts.count > 0
       puts "Bad counties: #{old_county_districts.inspect}"
       puts "#{TransitAsset.joins(:districts).where(districts: {id: old_county_districts}).count} assets use invalid counties"
