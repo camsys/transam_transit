49778f5fabc886eb5950598871fdcbfb1128c518
Alex Andrade <aandrade@camsys.com>
2020-01-14 17:08:18 -0500

    [TTPLAT-1254] Add categorization to query tool; remove facility categorization.

--- db/transit_asset_query_views.sql
+++ db/transit_asset_query_views.sql
@@ -46,18 +46,19 @@ CREATE OR REPLACE VIEW transit_assets_direct_capital_responsibility_view AS
     inner join transam_assets 
     on transam_assets.transam_assetible_id = transit_assets.id and transam_assets.transam_assetible_type = 'TransitAsset';
 
-DROP VIEW if exists facility_category_view;
-CREATE OR REPLACE VIEW facility_category_view AS
-    select transam_assets.id as transam_asset_id, (case when component_types.name is not NULL then 'Component' when component_subtypes.name is not NULL then 'Sub-Component' ELSE 'Primary Facility' END) as facility_category_name, 
-    (case when component_types.name is not NULL then component_types.name when component_subtypes.name is not NULL then component_subtypes.name ELSE NULL END) as facility_component_type
+DROP VIEW if exists transit_assets_category_view;
+CREATE OR REPLACE VIEW transit_assets_category_view AS
+    select transam_assets.id as transam_asset_id, (case when component_subtypes.name is not NULL then 'Sub-Component' when component_types.name is not NULL then 'Component' ELSE 'Primary' END) as categorization_name,
+    (case when component_subtypes.name is not NULL then component_subtypes.name when component_types.name is not NULL then component_types.name ELSE NULL END) as component_type_subtype_name
     FROM transam_assets
     LEFT JOIN transit_assets ON transit_assets.id = transam_assets.transam_assetible_id
     LEFT JOIN facilities on facilities.id = transit_assets.transit_assetible_id and transit_assets.transit_assetible_type = 'Facility'
+    LEFT JOIN infrastructures on infrastructures.id = transit_assets.transit_assetible_id and transit_assets.transit_assetible_type = 'Infrastructure'
     LEFT JOIN transit_components ON transit_components.id = transit_assets.transit_assetible_id
       AND transit_assets.transit_assetible_type = 'TransitComponent'
     LEFT JOIN component_types ON component_types.id = transit_components.component_type_id 
     LEFT JOIN component_subtypes on component_subtypes.id = transit_components.component_subtype_id 
-    WHERE transam_assets.transam_assetible_type = 'TransitAsset' AND ( facilities.id > 0 OR transit_assets.fta_asset_class_id IN ( SELECT fta_asset_classes.id FROM fta_asset_classes WHERE fta_asset_classes.class_name = 'Facility' ));
+    WHERE transam_assets.transam_assetible_type = 'TransitAsset' AND ( facilities.id > 0 OR infrastructures.id > 0 OR transit_assets.fta_asset_class_id IN ( SELECT fta_asset_classes.id FROM fta_asset_classes WHERE fta_asset_classes.class_name IN ('Facility', 'Guideway', 'PowerSignal', 'Track')));
 
 DROP VIEW if exists transit_components_element_measurement_view;
 CREATE OR REPLACE VIEW transit_components_element_measurement_view AS
