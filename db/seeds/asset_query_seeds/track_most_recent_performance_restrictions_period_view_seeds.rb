1cfaff003fab7fb1872a0ed2d987f277b020243e
Alex Andrade <aandrade@camsys.com>
2020-01-15 17:30:23 -0500

    [TTPLAT-1284] Add Period Length and Unit fields to query tool.

--- db/seeds/asset_query_seeds/track_most_recent_performance_restrictions_period_view_seeds.rb
+++ db/seeds/asset_query_seeds/track_most_recent_performance_restrictions_period_view_seeds.rb
@@ -19,11 +19,35 @@ data_table = QueryAssetClass.find_or_create_by(
     transam_assets_join: "LEFT JOIN track_most_recent_performance_restrictions_period_view on track_most_recent_performance_restrictions_period_view.base_transam_asset_id = transam_assets.id"
 )
 
-# query field
-qf = QueryField.find_or_create_by(
-    name: 'period',
-    label: 'Period',
-    filter_type: 'multi_select',
-    query_category: qc
-)
-qf.query_asset_classes = [data_table]
+#create query fields
+fields = [
+    {
+        name: 'period',
+        label: 'Period',
+        filter_type: 'multi_select'
+    },
+    {
+        name: 'period_length',
+        label: 'Period of Time',
+        filter_type: 'numeric',
+        pairs_with: 'period_length_unit'
+    },
+    {
+        name: 'period_length_unit',
+        label: 'Unit',
+        filter_type: 'text',
+        hidden: true
+    }
+]
+
+fields.each do |f|
+  qf = QueryField.find_or_create_by(
+      name: f[:name],
+      label: f[:label],
+      query_category: qc,
+      filter_type: f[:filter_type],
+      hidden: f[:hidden],
+      pairs_with: f[:pairs_with]
+  )
+  qf.query_asset_classes = [data_table]
+end
