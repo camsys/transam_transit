04855de5aa9a386b0489d87b8269d83cce245e40
Alex Andrade <aandrade@camsys.com>
2020-01-15 17:11:56 -0500

    [TTPLAT-1285] Create query tool most recent asset events view in seed file and migration.

--- db/seeds/asset_query_seeds/asset_event_seeds.rb
+++ db/seeds/asset_query_seeds/asset_event_seeds.rb
@@ -1,3 +1,15 @@
+# create most recent asset events view
+query_view_sql = <<-SQL
+  CREATE OR REPLACE VIEW query_tool_most_recent_asset_events_for_type_view AS
+    SELECT
+      aet.id AS asset_event_type_id, aet.name AS asset_event_name, Max(ae.created_at) AS asset_event_created_time, ae.base_transam_asset_id, Max(ae.id) AS asset_event_id
+    FROM asset_events AS ae
+    LEFT JOIN asset_event_types AS aet ON aet.id = ae.asset_event_type_id
+    LEFT JOIN transam_assets AS ta  ON ta.id = ae.base_transam_asset_id
+    GROUP BY aet.id, ae.base_transam_asset_id;
+SQL
+ActiveRecord::Base.connection.execute query_view_sql
+
 asset_events_table = QueryAssetClass.find_or_create_by(
   table_name: 'asset_events', 
   transam_assets_join: "left join asset_events on asset_events.transam_asset_id = transam_assets.id left join asset_event_types as ae_types on asset_events.asset_event_type_id = ae_types.id"
