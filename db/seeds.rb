74168ea92ec8e7fb28db725c633aaa0546648765
Derek Edwards <dedwards8@gmail.com>
2020-04-30 16:28:16 -0400

    TTPLAT-1489 Assets table (#225)
    * Bus Table Initial Commit
    
    * Temp
    
    * Temp commit, make bus table searchable on orgs and manufacturers
    
    * Search on manufacture year
    
    * Add searching on vehicle type, subtype, and esl category.
    
    * TTPLAT-1489 Reference FtaAssetClass by code instead of id.
    
    * Add code to fta asset class seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -468,19 +468,19 @@ fta_asset_categories = [
     {name: 'Infrastructure', display_icon_name: 'fa fa-road', active: true}
 ]
 fta_asset_classes = [
-    {fta_category: 'Revenue Vehicles', name: 'Buses (Rubber Tire Vehicles)', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-bus', active: true},
-    {fta_category: 'Revenue Vehicles', name: 'Rail Cars', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-subway', active: true},
-    {fta_category: 'Revenue Vehicles', name: 'Ferries', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-ship', active: true},
-    {fta_category: 'Revenue Vehicles', name: 'Other Passenger Vehicles', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-train', active: true},
-    {fta_category: 'Equipment', name: 'Service Vehicles (Non-Revenue)', class_name: 'ServiceVehicle', display_icon_name: 'fa fa-car', active: true},
-    {fta_category: 'Equipment', name: 'Capital Equipment', class_name: 'CapitalEquipment', display_icon_name: 'fa fa-map-signs', active: true},
-    {fta_category: 'Facilities', name: 'Administration', class_name: 'Facility', display_icon_name: 'fa fa-university', active: true},
-    {fta_category: 'Facilities', name: 'Maintenance', class_name: 'Facility', display_icon_name: 'fa fa-wrench', active: true},
-    {fta_category: 'Facilities', name: 'Passenger', class_name: 'Facility', display_icon_name: 'fa fa-user', active: true},
-    {fta_category: 'Facilities', name: 'Parking', class_name: 'Facility', display_icon_name: 'fa fa-road', active: true},
-    {fta_category: 'Infrastructure', name: 'Guideway', class_name: 'Guideway', display_icon_name: 'fa fa-map', active: true},
-    {fta_category: 'Infrastructure', name: 'Power & Signal', class_name: 'PowerSignal', display_icon_name: 'fa fa-plug', active: true},
-    {fta_category: 'Infrastructure', name: 'Track', class_name: 'Track', display_icon_name: 'fa fa-train', active: true}
+    {fta_category: 'Revenue Vehicles', code: "bus", name: 'Buses (Rubber Tire Vehicles)', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-bus', active: true},
+    {fta_category: 'Revenue Vehicles', code: "rail_car", name: 'Rail Cars', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-subway', active: true},
+    {fta_category: 'Revenue Vehicles', code: "ferry", name: 'Ferries', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-ship', active: true},
+    {fta_category: 'Revenue Vehicles', code: "other_passenger_vehicle", name: 'Other Passenger Vehicles', class_name: 'RevenueVehicle', display_icon_name: 'fa fa-train', active: true},
+    {fta_category: 'Equipment', code: "service_vehicle", name: 'Service Vehicles (Non-Revenue)', class_name: 'ServiceVehicle', display_icon_name: 'fa fa-car', active: true},
+    {fta_category: 'Equipment', code: "capital_equipment", name: 'Capital Equipment', class_name: 'CapitalEquipment', display_icon_name: 'fa fa-map-signs', active: true},
+    {fta_category: 'Facilities', code: "admin_facility", name: 'Administration', class_name: 'Facility', display_icon_name: 'fa fa-university', active: true},
+    {fta_category: 'Facilities', code: "maintenance_facility", name: 'Maintenance', class_name: 'Facility', display_icon_name: 'fa fa-wrench', active: true},
+    {fta_category: 'Facilities', code: "passenger_facility", name: 'Passenger', class_name: 'Facility', display_icon_name: 'fa fa-user', active: true},
+    {fta_category: 'Facilities', code: "parking_facility", name: 'Parking', class_name: 'Facility', display_icon_name: 'fa fa-road', active: true},
+    {fta_category: 'Infrastructure', code: "guideway", name: 'Guideway', class_name: 'Guideway', display_icon_name: 'fa fa-map', active: true},
+    {fta_category: 'Infrastructure', code: "power_signal", name: 'Power & Signal', class_name: 'PowerSignal', display_icon_name: 'fa fa-plug', active: true},
+    {fta_category: 'Infrastructure', code: "track", name: 'Track', class_name: 'Track', display_icon_name: 'fa fa-train', active: true}
 ]
 
 contract_types = [
