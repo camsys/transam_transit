0486bcd6cacc8b9fb7012cd5be1ffe946379931b
Derek Edwards <dedwards8@gmail.com>
2020-05-30 13:38:43 -0400

    Add search to track table api call.

--- app/controllers/transit_assets_controller.rb
+++ app/controllers/transit_assets_controller.rb
@@ -11,11 +11,60 @@ class TransitAssetsController < OrganizationAwareController
   # TODO: MOST of this will be moved to a shareable module
   #-----------------------------------------------------------------------------
   def table
+    code = params[:fta_asset_class_code]
+    
+    case code
+    when 'bus', 'rail_car', 'ferry', 'other_passenger_vehicle'
+      response = revenue_vehicles_table
+    when 'track'
+      response = track_table
+    else
+      response = {count: 0, rows: []}
+    end
+
+    render status: 200, json: response
+  end
+
+  def track_table
+    tracks = Track.all
+    page = (table_params[:page] || 0).to_i
+    page_size = (table_params[:page_size] || tracks.count).to_i
+    search = (table_params[:search]) 
+    offset = page*page_size
+
+    query = nil 
+    if search
+      search_string = "%#{search}%"
+      query = track_query_builder(search_string)
+              .or(org_query search_string)
+              .or(fta_subtype_query search_string)
+              .or(infrastructure_division_query search_string)
+              .or(infrastructure_subdivision_query search_string)
+              .or(infrastructure_track_query search_string)
+              .or(infrastructure_segment_type_query search_string)
+
+      tracks = Track.joins('left join organizations on organization_id = organizations.id')
+               .joins('left join asset_subtypes on asset_subtype_id = asset_subtypes.id')
+               .joins('left join infrastructure_divisions on infrastructure_division_id = infrastructure_divisions.id')
+               .joins('left join infrastructure_subdivisions on infrastructure_subdivision_id = infrastructure_subdivisions.id')
+               .joins('left join infrastructure_tracks on infrastructure_track_id = infrastructure_tracks.id')
+               .joins('left join infrastructure_segment_types on infrastructure_segment_type_id = infrastructure_segment_types.id')
+               .where(query)
+    else
+      tracks = Track.all 
+    end
+    
+    asset_table = tracks.offset(offset).limit(page_size).map{ |a| a.rowify }
+    
+    return {count: tracks.count, rows: asset_table}
+  end
+
+  def revenue_vehicles_table
     fta_asset_class = FtaAssetClass.find_by(code: params[:fta_asset_class_code])
     fta_asset_class_id = fta_asset_class.id 
-    vehicles = RevenueVehicle.where(fta_asset_class_id: fta_asset_class_id) #All Buses
+    vehicles = RevenueVehicle.where(fta_asset_class_id: fta_asset_class_id) 
     page = (table_params[:page] || 0).to_i
-    page_size = (table_params[:page_size] || count).to_i
+    page_size = (table_params[:page_size] || vehicles.count).to_i
     search = (table_params[:search]) 
     offset = page*page_size
 
@@ -31,7 +80,6 @@ class TransitAssetsController < OrganizationAwareController
               .or(fta_subtype_query search_string)
               .or(esl_category_query search_string)
 
-      index = 0
       vehicles = RevenueVehicle.joins('left join organizations on organization_id = organizations.id')
                                .joins('left join manufacturers on manufacturer_id = manufacturers.id')
                                .joins('left join fta_vehicle_types on fta_type_id = fta_vehicle_types.id')
@@ -45,8 +93,12 @@ class TransitAssetsController < OrganizationAwareController
     else 
       asset_table = RevenueVehicle.where(fta_asset_class_id: fta_asset_class_id).offset(offset).limit(page_size).map{ |a| a.very_specific.rowify }
     end
-    render status: 200, json: {count: vehicles.count, rows: asset_table}
-  end
+    
+    return {count: vehicles.count, rows: asset_table}
+  
+  end 
+
+  protected
 
   def is_number? string
     true if Float(string) rescue false
@@ -72,9 +124,35 @@ class TransitAssetsController < OrganizationAwareController
     EslCategory.arel_table[:name].matches(search_string)
   end
 
+  def infrastructure_division_query search_string
+    InfrastructureDivision.arel_table[:name].matches(search_string)
+  end
+
+  def infrastructure_subdivision_query search_string
+    InfrastructureSubdivision.arel_table[:name].matches(search_string)
+  end
+
+  def infrastructure_track_query search_string
+    InfrastructureTrack.arel_table[:name].matches(search_string)
+  end
+
+  def infrastructure_segment_type_query search_string
+    InfrastructureSegmentType.arel_table[:name].matches(search_string)
+  end
+
+  def track_query_builder search_string
+    query = TransamAsset.arel_table[:asset_tag].matches(search_string)
+            .or(TransamAsset.arel_table[:description].matches(search_string))
+            .or(Infrastructure.arel_table[:from_line].matches(search_string))
+            .or(Infrastructure.arel_table[:to_line].matches(search_string))
+            .or(Infrastructure.arel_table[:from_segment].matches(search_string))
+            .or(Infrastructure.arel_table[:to_segment].matches(search_string))
+            .or(Infrastructure.arel_table[:relative_location].matches(search_string))
+  end
+
   def query_builder search_string, search_year 
     query = TransamAsset.arel_table[:asset_tag].matches(search_string)
-    .or(ServiceVehicle.arel_table[:serial_number].matches(search_string))
+            .or(ServiceVehicle.arel_table[:serial_number].matches(search_string))
 
     if search_year 
       query = query.or(TransamAsset.arel_table[:manufacture_year].matches(search_year))
