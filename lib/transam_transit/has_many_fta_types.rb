66f08dfcdb2242915c3cbaee5049b11a1a3e2b65
W. Scott Meeks <smeeks@camsys.com>
2021-02-02 12:29:55 -0500

    TTPLAT-1850 added scope, added browser side limitation of class/type/subtype options

--- lib/transam_transit/has_many_fta_types.rb
+++ lib/transam_transit/has_many_fta_types.rb
@@ -10,6 +10,11 @@ module HasManyFtaTypes
 
   included do
     has_many :fta_type_asset_subtype_mappings
+    
+    scope :with_fta_type, ->(type) {
+      joins(:fta_type_asset_subtype_mappings)
+        .where(fta_type_asset_subtype_mappings: {fta_type_type: type.class.name, fta_type_id: type.id})
+    }
 
     def fta_types
       fta_type_asset_subtype_mappings.map{|m| m.fta_type}
