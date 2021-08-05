3306120184f6eb0c961cd757f6d2e2359a266c6e
Derek Edwards <dedwards8@gmail.com>
2021-08-05 16:53:34 -0400

    Update cors headers

--- app/controllers/inventory_api/v1/assets_controller.rb
+++ app/controllers/inventory_api/v1/assets_controller.rb
@@ -1,6 +1,6 @@
 class InventoryApi::V1::AssetsController < ApplicationController
 
-  before_filter :cors_set_access_control_headers
+  before_action :cors_set_access_control_headers
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
